dce11492e4416226be0ee6ed35d14dc29374a20b
Oliver Palmer <oliverpalmer@opalmer.com>
2013-09-19 23:46:45 -0400

    Python 2.6 fix

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ else:
 
 setup(
     name="pyfarm.core",
-    version="0.7.0-dev4",
+    version="0.7.0-dev5",
     packages=["pyfarm",
               "pyfarm.core",
               "pyfarm.core.app",
