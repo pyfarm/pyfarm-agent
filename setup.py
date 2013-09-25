8b8b41c727c99e4f0b50f0db35326de2eede5317
Oliver Palmer <oliverpalmer@opalmer.com>
2013-09-24 23:28:45 -0400

    version up

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ else:
 
 setup(
     name="pyfarm.core",
-    version="0.7.0-dev7",
+    version="0.7.0-dev8",
     packages=["pyfarm",
               "pyfarm.core",
               "pyfarm.core.sysinfo"],
