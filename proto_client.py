12bd8ac405e08700d2b4558a39966bfebd600fbf
Oliver Palmer <oliverpalmer@opalmer.com>
2013-10-26 03:01:50 -0400

    WIP

--- proto_client.py
+++ proto_client.py
@@ -1,8 +1,15 @@
 
-from pyfarm.agent.utility import unpack_result_tuple
+from pyfarm.agent.utility import unpack_result_tuple, pack_result_tuple
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("127.0.0.1", 9000))
-s.send("foo?")
-print unpack_result_tuple(s.recv(1024))
+
+from pyfarm.agent.ipc_pb2 import IPCMessage
+message = IPCMessage()
+message.code = 1
+s.send(message.SerializeToString())
+response = IPCMessage()
+response.ParseFromString((s.recv(2048)))
+
+print response.code
 s.close()
--- proto_server.py
+++ proto_server.py
@@ -1,36 +1,65 @@
 from twisted.python import log
 from twisted.application.service import Application
-from twisted.internet.protocol import Protocol, Factory
+from twisted.internet.protocol import Factory, Protocol
+from twisted.protocols.basic import LineReceiver
 from twisted.internet.endpoints import TCP4ServerEndpoint
 from twisted.internet import reactor
 
-from pyfarm.agent.utility import pack_result_tuple
+from pyfarm.agent.ipc_pb2 import IPCMessage
 
 
-class ReceieverProtocol(Protocol):
+class ProtobufProtocol(LineReceiver):
+    protobuf = NotImplemented
+    line_mode = 0
+
+    def rawDataReceived(self, data):
+        raise NotImplementedError
+
+
+class IPCReceiever(ProtobufProtocol):
+    protobuf = IPCMessage
+
+    def rawDataReceived(self, data):
+        self.factory.known_hosts.add(self.transport.client[0])
+
+        try:
+            pb = self.protobuf()
+            pb.ParseFromString(data)
+
+        except Exception, e:  # TODO: be more explicit about errors
+            def next():
+                # TODO: add more information
+                pb = self.protobuf()
+                pb.code = 1
+                pb.error.classname = e.__class__.__name__
+                self.transport.write(pb.SerializeToString())
+                self.transport.loseConnection()
+                log.err(
+                    "failed to handle message from %s:%s" %
+                    self.transport.client)
+
+        else:
+            def next():
+                pb = self.protobuf()
+                self.transport.write(pb.SerializeToString())
+                self.transport.loseConnection()
+                log.msg("handled message from %s:%s" % self.transport.client)
+
+        reactor.callLater(0, next)
+
+
+
+
+class IPCReceieverFactory(Factory):
+    protocol = IPCReceiever
     known_hosts = set()
 
-    def connectionMade(self):
-        address, port = self.transport.client
-
-        # log and keep track of new hosts
-        if address not in self.known_hosts:
-            log.msg("new connection from %s received" % address)
-            self.known_hosts.add(address)
-
-    def dataReceived(self, data):
-        self.transport.write(pack_result_tuple(0, data))
-
-
-class ReceieverFactory(Factory):
-    protocol = ReceieverProtocol
-
     def stopFactory(self):  # TODO: notify all connected hosts we are stopping
-        if self.protocol.known_hosts:
+        if self.known_hosts:
             log.msg("notifying all known hosts of termination")
 
 
 endpoint = TCP4ServerEndpoint(reactor, 9000)
-endpoint.listen(ReceieverFactory())
+endpoint.listen(IPCReceieverFactory())
 
 application = Application("server")
