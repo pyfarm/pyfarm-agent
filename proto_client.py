11e306a709f74e80b4630acd712b66aa8308c384
Oliver Palmer <oliverpalmer@opalmer.com>
2013-10-27 08:16:34 -0400

    WIP

--- proto_client.py
+++ proto_client.py
@@ -1,15 +1,18 @@
 
 from pyfarm.agent.utility import unpack_result_tuple, pack_result_tuple
 import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("127.0.0.1", 9000))
 
-from pyfarm.agent.ipc_pb2 import IPCMessage
-message = IPCMessage()
-message.code = 1
-s.send(message.SerializeToString())
-response = IPCMessage()
-response.ParseFromString((s.recv(2048)))
+while True:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(("127.0.0.1", 50000))
 
-print response.code
-s.close()
+    from pyfarm.agent.ipc_pb2 import IPCMessage
+    message = IPCMessage()
+    message.code = 1
+    s.send(message.SerializeToString())
+    response = IPCMessage()
+    response.ParseFromString((s.recv(2048)))
+
+    #print response.code
+    s.close()
+    break
