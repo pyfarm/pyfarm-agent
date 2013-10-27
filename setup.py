3459d3df719973fa1ff130b7ddca2d76e959c21c
Oliver Palmer <oliverpalmer@opalmer.com>
2013-10-27 14:14:35 -0400

    post-release commit

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ else:
 
 setup(
     name="pyfarm.core",
-    version="0.7.0-dev11",
+    version="0.7.0-dev12",
     packages=["pyfarm",
               "pyfarm.core",
               "pyfarm.core.sysinfo"],
