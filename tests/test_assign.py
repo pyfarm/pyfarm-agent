0b9a9d0f00451d87635cd21469af2ec07d218675
Oliver Palmer <oliverpalmer@opalmer.com>
2013-12-29 12:25:07 -0500

    adding basic tests for the source code parser

--- tests/test_assign.py
+++ tests/test_assign.py
@@ -20,22 +20,60 @@ import shutil
 import tempfile
 from StringIO import StringIO
 
-from voluptuous import Schema, Invalid
+from twisted.trial.unittest import TestCase
+from voluptuous import Schema, Invalid, MultipleInvalid
 
-from utcore import BaseTestCase
 from pyfarm.core.enums import JobTypeLoadMode
 from pyfarm.agent.http.assign import PostProcessedSchema, Assign
 
 
-class TestSchema(BaseTestCase):
-    VALID_ASSIGNMENT_DATA = {
-        "project": 0, "job": 1, "task": 0,
-        "jobtype": {
-            "load_type": JobTypeLoadMode.IMPORT,
-            "load_from": "",
-            "cmd": "", "args": ""},
-        "frame": {"start": 1}}
+class TestSchema(TestCase):
+    def get_test_data(self):
+        return {
+            "project": 0, "job": 0, "task": 0,
+            "jobtype": {
+                "load_type": JobTypeLoadMode.IMPORT,
+                "load_from": "import_name:ClassName",
+                "cmd": "", "args": ""},
+            "frame": {"start": 1}}
 
+    def test_schema_subclass(self):
+        self.assertTrue(issubclass(PostProcessedSchema, Schema))
+
+    def test_instance(self):
+        self.assertIsInstance(Assign.SCHEMA, PostProcessedSchema)
+
+    def test_frame_data_population(self):
+        test_data = self.get_test_data()
+        test_data["frame"] = {"start": 1}
+        data = Assign.SCHEMA(test_data, {}, parse_jobtype=False)
+        self.assertEqual(data["frame"]["start"], 1)
+        self.assertEqual(data["frame"]["end"], 1)
+        self.assertEqual(data["frame"]["by"], 1)
+
+    def test_valid_jobtype_load_mode(self):
+        test_data = self.get_test_data()
+        test_data["jobtype"]["load_type"] = ""
+        self.assertRaises(
+            MultipleInvalid,
+            lambda: Assign.SCHEMA(test_data, {}, parse_jobtype=False))
+
+    def test_strings_values_only(self):
+        self.assertEqual(
+            PostProcessedSchema.string_keys_and_values({"": ""}),
+            {"": ""})
+        self.assertRaisesRegexp(
+            Invalid, "expected string for env value",
+            lambda: PostProcessedSchema.string_keys_and_values({"": None}))
+        self.assertRaisesRegexp(
+            Invalid, "expected string for env key",
+            lambda: PostProcessedSchema.string_keys_and_values({None: ""}))
+        self.assertRaisesRegexp(
+            Invalid, "invalid type",
+            lambda: PostProcessedSchema.string_keys_and_values(None))
+
+
+class TestJobTypeValidation(TestSchema):
     def setUp(self):
         self.import_dir = tempfile.mkdtemp(prefix="pyfarm-agent-tests-")
 
@@ -48,7 +86,6 @@ class TestSchema(BaseTestCase):
         with open(os.path.join(self.import_dir, "__init__.py"), "w") as stream:
             stream.write("")
 
-        self.test_data = self.VALID_ASSIGNMENT_DATA.copy()
         sys.path.insert(0, self.import_dir)
 
     def tearDown(self):
@@ -60,7 +97,8 @@ class TestSchema(BaseTestCase):
 
     def generate_module(
             self, classname="JobType", subclass_base=True, syntax_error=False,
-            call_in_module=False, exec_in_module=False, eval_in_module=False):
+            call_in_module=False, exec_in_module=False, eval_in_module=False,
+            missing_class=False):
         sourcecode = StringIO()
 
         if subclass_base:
@@ -71,9 +109,10 @@ class TestSchema(BaseTestCase):
         else:
             base_class = "object"
 
-        print >> sourcecode, "class %s(%s):" % (classname, base_class)
-        print >> sourcecode, "    pass"
-        print >> sourcecode, ""
+        if not missing_class:
+            print >> sourcecode, "class %s(%s):" % (classname, base_class)
+            print >> sourcecode, "    pass"
+            print >> sourcecode, ""
 
         if syntax_error:
             print >> sourcecode, "a ="
@@ -95,31 +134,79 @@ class TestSchema(BaseTestCase):
 
         return ":".join([module_name, classname])
 
-    def test_schema_subclass(self):
-        self.assertTrue(issubclass(PostProcessedSchema, Schema))
+    def test_invalid_load_from_format(self):
+        test_data = self.get_test_data()
+        test_data["jobtype"]["load_from"] = ""
+        self.assertRaisesRegexp(
+            Invalid, "does not match the 'import_name:ClassName' format",
+            lambda: Assign.SCHEMA(test_data, {}))
 
-    def test_instance(self):
-        self.assertIsInstance(Assign.SCHEMA, PostProcessedSchema)
-
-    def test_strings_values_only(self):
-        self.assertRaises(
-            Invalid,
-            lambda: PostProcessedSchema.string_keys_and_values({"": None}))
-        self.assertRaises(
-            Invalid,
-            lambda: PostProcessedSchema.string_keys_and_values({None: ""}))
-
-        self.assertRaises(
-            Invalid,
-            lambda: PostProcessedSchema.string_keys_and_values(None))
-
-    def test_invalid_load_from(self):
-        self.test_data["jobtype"]["load_from"] = ""
-        self.assertRaises(Invalid, lambda: Assign.SCHEMA(self.test_data, {}))
+    def test_invalid_import(self):
+        test_data = self.get_test_data()
+        test_data["jobtype"]["load_from"] = "a:b"
+        self.assertRaisesRegexp(
+            Invalid, "no such jobtype module a:b",
+            lambda: Assign.SCHEMA(test_data, {}))
 
     def test_basic_import(self):
-        self.test_data["jobtype"]["load_type"] = JobTypeLoadMode.IMPORT
-        self.test_data["jobtype"]["load_from"] = self.generate_module()
-        assign = Assign.SCHEMA(self.test_data, {})
+        test_data = self.get_test_data()
+        test_data["jobtype"]["load_type"] = JobTypeLoadMode.IMPORT
+        test_data["jobtype"]["load_from"] = self.generate_module()
+        assign = Assign.SCHEMA(test_data, {})
         self.assertIn("jobtype", assign["jobtype"])
 
+    def test_syntax_error(self):
+        test_data = self.get_test_data()
+        test_data["jobtype"]["load_type"] = JobTypeLoadMode.IMPORT
+        test_data["jobtype"]["load_from"] = self.generate_module(
+            syntax_error=True)
+        self.assertRaisesRegexp(
+            Invalid, "invalid syntax",
+            lambda: Assign.SCHEMA(test_data, {}))
+
+    def test_call_in_module(self):
+        test_data = self.get_test_data()
+        test_data["jobtype"]["load_type"] = JobTypeLoadMode.IMPORT
+        test_data["jobtype"]["load_from"] = self.generate_module(
+            call_in_module=True)
+        self.assertRaisesRegexp(
+            Invalid, "function calls are not allowed",
+            lambda: Assign.SCHEMA(test_data, {}))
+
+    def test_exec_in_module(self):
+        test_data = self.get_test_data()
+        test_data["jobtype"]["load_type"] = JobTypeLoadMode.IMPORT
+        test_data["jobtype"]["load_from"] = self.generate_module(
+            exec_in_module=True)
+        self.assertRaisesRegexp(
+            Invalid, "function calls are not allowed",
+            lambda: Assign.SCHEMA(test_data, {}))
+
+    def test_eval_in_module(self):
+        test_data = self.get_test_data()
+        test_data["jobtype"]["load_type"] = JobTypeLoadMode.IMPORT
+        test_data["jobtype"]["load_from"] = self.generate_module(
+            eval_in_module=True)
+        self.assertRaisesRegexp(
+            Invalid, "function calls are not allowed",
+            lambda: Assign.SCHEMA(test_data, {}))
+
+    def test_missing_class(self):
+        test_data = self.get_test_data()
+        test_data["jobtype"]["load_type"] = JobTypeLoadMode.IMPORT
+        test_data["jobtype"]["load_from"] = self.generate_module(
+            missing_class=True)
+
+        self.assertRaisesRegexp(
+            Invalid, "does not have the class",
+            lambda: Assign.SCHEMA(test_data, {}))
+
+    def test_invalid_subclass(self):
+        test_data = self.get_test_data()
+        test_data["jobtype"]["load_type"] = JobTypeLoadMode.IMPORT
+        test_data["jobtype"]["load_from"] = self.generate_module(
+            subclass_base=False)
+
+        self.assertRaisesRegexp(
+            Invalid, "does not subclass base class",
+            lambda: Assign.SCHEMA(test_data, {}))
