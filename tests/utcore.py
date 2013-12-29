1d58b89d5f4a6d193e8aabfcc7d8d18a30f340a9
Oliver Palmer <oliverpalmer@opalmer.com>
2013-12-29 14:11:35 -0500

    adding DummyRequest wrapper

--- tests/utcore.py
+++ tests/utcore.py
@@ -16,7 +16,26 @@
 
 import re
 
+from twisted.web.server import NOT_DONE_YET
+from twisted.internet.defer import succeed
+from twisted.web.test.test_web import DummyRequest as _DummyRequest
 from twisted.trial.unittest import TestCase as _TestCase
+
+
+def dummy_request(path="", http_method="GET", headers=None, session=None):
+    """
+    Wrapper around the base dummy request which does not require direct
+    subclassing for different kinds of request or lists for a single
+    url request.
+    """
+    class DummyRequest(_DummyRequest):
+        method = http_method
+
+    request = DummyRequest([path], session=session)
+    if isinstance(headers, dict):
+        for key, value in headers.iteritems():
+            request.setHeader(key, value)
+    return request
 
 
 class TestCase(_TestCase):
@@ -42,3 +61,20 @@ class TestCase(_TestCase):
             if not expected_regexp.search(str(exception)):
                 self.fail('"%s" does not match "%s"' % (
                     expected_regexp.pattern, str(exception)))
+
+    def _render(self, resource, request):
+        result = resource.render(request)
+
+        if isinstance(result, str):
+            request.write(result)
+            request.finish()
+            return succeed(None)
+
+        elif result is NOT_DONE_YET:
+            if request.finished:
+                return succeed(None)
+
+            else:
+                return request.notifyFinish()
+        else:
+            raise ValueError("Unexpected return value: %r" % (result,))
