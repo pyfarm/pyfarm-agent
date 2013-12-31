ebb74c4aed89605858d171385db619431f6004c9
Oliver Palmer <oliverpalmer@opalmer.com>
2013-12-31 11:50:37 -0500

    adding pyfarm.jobtypes dependency, removing explicit installs in .travis.yml, fixing issue in twistd plugin install

--- setup.py
+++ setup.py
@@ -25,7 +25,8 @@ from os.path import isfile, join
 from setuptools import setup
 
 install_requires = [
-    "pyfarm.core", "psutil", "netifaces", "netaddr", "twisted", "statsd",
+    "pyfarm.core", "pyfarm.jobtypes",
+    "psutil", "netifaces", "netaddr", "twisted", "statsd",
     "PyOpenSSL", "ntplib", "requests", "txtemplate", "voluptuous"]
 
 if sys.version_info[0:2] < (2, 7):
@@ -36,6 +37,42 @@ if isfile("README.rst"):
         long_description = readme.read()
 else:
     long_description = ""
+
+
+# source: http://stackoverflow.com/a/7525163
+# When pip installs anything from packages, py_modules, or ext_modules that
+# includes a twistd plugin (which are installed to twisted/plugins/),
+# setuptools/distribute writes a Package.egg-info/top_level.txt that includes
+# "twisted".  If you later uninstall Package with `pip uninstall Package`,
+# pip <1.2 removes all of twisted/ instead of just Package's twistd plugins.
+# See https://github.com/pypa/pip/issues/355 (now fixed)
+#
+# To work around this problem, we monkeypatch
+# setuptools.command.egg_info.write_toplevel_names to not write the line
+# "twisted".  This fixes the behavior of `pip uninstall Package`.  Note that
+# even with this workaround, `pip uninstall Package` still correctly uninstalls
+# Package's twistd plugins from twisted/plugins/, since pip also uses
+# Package.egg-info/installed-files.txt to determine what to uninstall,
+# and the paths to the plugin files are indeed listed in installed-files.txt.
+try:
+    from setuptools.command import egg_info
+    egg_info.write_toplevel_names
+except (ImportError, AttributeError):
+    pass
+else:
+    def _top_level_package(name):
+        return name.split('.', 1)[0]
+
+    def _hacked_write_toplevel_names(cmd, basename, filename):
+        pkgs = dict.fromkeys(
+            [_top_level_package(k)
+                for k in cmd.distribution.iter_distribution_names()
+                if _top_level_package(k) != "twisted"
+            ]
+        )
+        cmd.write_file("top-level names", filename, '\n'.join(pkgs) + '\n')
+
+    egg_info.write_toplevel_names = _hacked_write_toplevel_names
 
 
 def get_package_data():
@@ -62,8 +99,7 @@ setup(
               "pyfarm.agent.process",
               "pyfarm.agent.utility"],
     package_data={
-        "pyfarm.agent": get_package_data(),
-        "twisted": [join("twisted", "pyfarm_plugin.py")]},
+        "pyfarm.agent": get_package_data()},
     namespace_packages=["pyfarm"],
     include_package_data=True,
     install_requires=install_requires,
@@ -87,12 +123,12 @@ setup(
         "Topic :: System :: Distributed Computing"])
 
 
-# though not always true, touching the plugin cache can
-# sometimes ensure our plugins show up in twistd
+# Make Twisted regenerate the dropin.cache, if possible.  This is necessary
+# because in a site-wide install, dropin.cache cannot be rewritten by
+# normal users.
 try:
-    import twisted.plugins
-    from twisted.plugin import getCache
-    getCache(twisted.plugins)
-
+    from twisted.plugin import IPlugin, getPlugins
 except ImportError:
     pass
+else:
+    list(getPlugins(IPlugin))
