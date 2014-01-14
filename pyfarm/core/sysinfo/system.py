983dfd69ec85d2e382ebb3b5b21a469098780cf7
Oliver Palmer <oliverpalmer@opalmer.com>
2014-01-13 22:11:44 -0500

    moved constants to enums.py, renamed top level functions to better represent use, fixed system arch. function to work better across platforms

--- pyfarm/core/sysinfo/system.py
+++ pyfarm/core/sysinfo/system.py
@@ -21,49 +21,6 @@ System
 Information about the operating system including type, filesystem information,
 and other relevant information.  This module may also contain os specific
 information such as the Linux distribution, Windows version, bitness, etc.
-
-:const OS:
-    Integer containing the os type, this is a mapping to a value
-    on :class:`OperatingSystem`
-
-:const IS_LINUX:
-    set to True if running linux
-
-:const IS_WINDOWS:
-    set to True if running windows
-
-:const IS_MAC:
-    set to True if running mac os
-
-:const IS_OTHER:
-    set to True if running something we could not determine a mapping for
-
-:const IS_POSIX:
-    set to True if running a posix platform (such as linux or mac)
-
-:const CASE_SENSITIVE_FILESYSTEM:
-    set to True if the filesystem is case sensitive
-
-:const CASE_SENSITIVE_ENVIRONMENT:
-    set to True if the environment is case sensitive
-
-:const ARCHITECTURE:
-    the architecture if the machine itself as an integer
-
-:const ARCHITECTURE32:
-    set to ``True`` when the system architecture is 32 bits
-
-:const ARCHITECTURE64:
-    set to ``True`` when the system architecture is 64 bits
-
-:const INTERPRETER_ARCHITECTURE:
-    the architecture of the Python interpreter as an integer
-
-:const INTERPRETER_ARCHITECTURE32:
-    set to ``True`` when the Python interpreter is 32 bits
-
-:const INTERPRETER_ARCHITECTURE64:
-    set to ``True`` when the Python interpreter is 64 bits
 """
 
 import os
@@ -72,46 +29,65 @@ import sys
 import time
 import tempfile
 import uuid
+from os.path import isfile
+from warnings import warn
 
 import psutil
 
-from pyfarm.core.enums import OS, OperatingSystem
+try:
+    WindowsError
+except NameError:
+    WindowsError = OSError
 
 
-def _case_sensitive_filesystem():  # pragma: no cover
+def filesystem_is_case_sensitive():  # pragma: no cover
     """returns True if the file system is case sensitive"""
     fd, path = tempfile.mkstemp()
-    if path.islower() or not path.isupper():
-        return os.path.isfile(path) and not os.path.isfile(path.upper())
-    else:
-        return os.path.isfile(path) and not os.path.isfile(path.lower())
+    case_sensitive = not all(map(isfile, [path, path.lower(), path.upper()]))
+
+    try:
+        os.remove(path)
+    except (WindowsError, OSError, NotImplementedError):
+        pass
+
+    return case_sensitive
 
 
-def _case_sensitive_environment():  # pragma: no cover
+def environment_is_case_sensitive():  # pragma: no cover
     """returns True if the environment is case sensitive"""
     envvar_lower = "PYFARM_CHECK_ENV_CASE_" + uuid.uuid4().hex
     envvar_upper = envvar_lower.upper()
 
-    # populate environment then compare the difference (clean afterwards)
+    # populate environment then compare the difference
     os.environ.update({envvar_lower: "0", envvar_upper: "1"})
-    result = os.environ[envvar_lower] != os.environ[envvar_upper]
-    map(os.environ.pop, (envvar_lower, envvar_upper))
+    case_sensitive = os.environ[envvar_lower] != os.environ[envvar_upper]
 
-    return result
+    # remove the envvars we just made
+    for envvar in (envvar_lower, envvar_upper):
+        os.environ.pop(envvar, None)
+
+    return case_sensitive
 
 
-def _system_architecture():
-    bitness, linkage = platform.architecture()
-
-    if bitness == "64bit":
+def machine_architecture():
+    """returns the architecture of the host itself"""
+    arch = platform.machine().lower()
+    if arch in ("amd64", "x86_64", "ia64") or "wow64" in arch:
         return 64
-    elif bitness == "32bit":
+
+    elif arch in ("i386", "i686", "x86"):
         return 32
+
+    elif not arch:
+        raise NotImplementedError(
+            "no result from `platform.machine` on this system")
+
     else:
-        raise ValueError("unknown bitness %s" % bitness)
+        raise NotImplementedError(
+            "don't know how to handle a machine architecture %s" % repr(arch))
 
 
-def _interpreter_architecture():
+def interpreter_architecture():
     """returns the architecture of the interpreter itself (32 or 64)"""
     if hasattr(sys, "maxsize"):
         if sys.maxsize > 2**32:
@@ -133,16 +109,14 @@ def uptime():
     return time.time() - psutil.BOOT_TIME
 
 
-IS_LINUX = OS == OperatingSystem.LINUX
-IS_WINDOWS = OS == OperatingSystem.WINDOWS
-IS_MAC = OS == OperatingSystem.MAC
-IS_OTHER = OS == OperatingSystem.OTHER
-IS_POSIX = OS in (OperatingSystem.LINUX, OperatingSystem.MAC)
-CASE_SENSITIVE_FILESYSTEM = _case_sensitive_filesystem()
-CASE_SENSITIVE_ENVIRONMENT = _case_sensitive_environment()
-ARCHITECTURE = _system_architecture()
-ARCHITECTURE64 = ARCHITECTURE == 64
-ARCHITECTURE32 = ARCHITECTURE == 32
-INTERPRETER_ARCHITECTURE = _interpreter_architecture()
-INTERPRETER_ARCHITECTURE32 = INTERPRETER_ARCHITECTURE == 32
-INTERPRETER_ARCHITECTURE64 = INTERPRETER_ARCHITECTURE == 64
+def operating_system(plat=sys.platform):
+    """returns the operating system for the given platform"""
+    if plat.startswith("linux"):
+        return "linux"
+    elif plat.startswith("win"):
+        return "windows"
+    elif plat.startswith("darwin"):
+        return "mac"
+    else:
+        warn("unknown operating system: %s" % plat)
+        return "other"
