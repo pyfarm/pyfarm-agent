308e8b15accb4be04a5cea6adf765f73e4e29d4d
Oliver Palmer <oliverpalmer@opalmer.com>
2014-02-21 00:43:19 -0500

    Python 2.6 fix, adding backported captureWarnings for #18

--- pyfarm/core/logger.py
+++ pyfarm/core/logger.py
@@ -29,8 +29,66 @@ message formatting.
 import sys
 import logging
 from logging import (
-    DEBUG, INFO, WARNING, ERROR, CRITICAL, getLogger as _getLogger,
-    captureWarnings)
+    DEBUG, INFO, WARNING, ERROR, CRITICAL, getLogger as _getLogger)
+
+from pyfarm.core.enums import PY26
+
+try:
+    from logging import NullHandler
+except ImportError:
+    from logutils import NullHandler
+
+try:
+    from logging import captureWarnings
+
+# Python 2.6 backport of captureWarnings
+except ImportError:
+    import warnings
+    _warnings_showwarning = None
+
+    def _showwarning(message, category, filename, lineno, file=None, line=None):
+        """
+        .. note::
+            This function is a copy of Python 2.7's ``_showwarning``
+
+        Implementation of showwarnings which redirects to logging, which will
+        first check to see if the file parameter is None. If a file is
+        specified, it will delegate to the original warnings implementation of
+        showwarning. Otherwise, it will call warnings.formatwarning and will
+        log the resulting string to a warnings logger named "py.warnings" with
+        level logging.WARNING.
+        """
+        if file is not None:
+            if _warnings_showwarning is not None:
+                _warnings_showwarning(
+                    message, category, filename, lineno, file, line)
+        else:
+            s = warnings.formatwarning(
+                message, category, filename, lineno, line)
+            logger = _getLogger("py.warnings")
+            if not logger.handlers:
+                logger.addHandler(NullHandler())
+            logger.warning("%s", s)
+
+    def captureWarnings(capture):
+        """
+        .. note::
+            This function is a copy of Python 2.7's ``captureWarnings``
+
+        If capture is true, redirect all warnings to the logging package.
+        If capture is False, ensure that warnings are not redirected to logging
+        but to their original destinations.
+        """
+        global _warnings_showwarning
+        if capture:
+            if _warnings_showwarning is None:
+                _warnings_showwarning = warnings.showwarning
+                warnings.showwarning = _showwarning
+        else:
+            if _warnings_showwarning is not None:
+                warnings.showwarning = _warnings_showwarning
+                _warnings_showwarning = None
+
 
 captureWarnings(True)
 
@@ -46,8 +104,6 @@ try:
 except ImportError:
     from logutils.dictconfig import dictConfig
 
-# grab the proper formatter
-from pyfarm.core.enums import PY26
 if not PY26:
     from logging import Formatter
 else:
@@ -71,9 +127,11 @@ class ColorFormatter(Formatter):
         else:
             return message
 
+# TODO: use dictConfig() to set this up
 HANDLER = logging.StreamHandler(sys.stdout)
 HANDLER.setFormatter(ColorFormatter())
 
+# TODO: use dictConfig() to set this up
 root = logging.getLogger()
 root.addHandler(HANDLER)
 root.setLevel(DEBUG)
