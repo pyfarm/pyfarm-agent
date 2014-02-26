8e460274811d69d8131f2c3d40bc75dc5a3cb8a6
Oliver Palmer <oliverpalmer@opalmer.com>
2014-02-25 23:08:57 -0500

    added TestPartial

---
+++ tests/test_agent/test_http_client.py
@@ -0,0 +1,46 @@
+# No shebang line, this module is meant to be imported
+#
+# Copyright 2014 Oliver Palmer
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from pyfarm.agent.testutil import TestCase
+from pyfarm.agent.http.client import (
+    request, head, get, post, put, patch, delete)
+
+
+class TestPartials(TestCase):
+    def test_head(self):
+        self.assertIs(head.func, request)
+        self.assertEqual(head.args, ("HEAD", ))
+
+    def test_get(self):
+        self.assertIs(get.func, request)
+        self.assertEqual(get.args, ("GET", ))
+
+    def test_post(self):
+        self.assertIs(post.func, request)
+        self.assertEqual(post.args, ("POST", ))
+
+    def test_put(self):
+        self.assertIs(put.func, request)
+        self.assertEqual(put.args, ("PUT", ))
+
+    def test_patch(self):
+        self.assertIs(patch.func, request)
+        self.assertEqual(patch.args, ("PATCH", ))
+
+    def test_delete(self):
+        self.assertIs(delete.func, request)
+        self.assertEqual(delete.args, ("DELETE", ))
+
