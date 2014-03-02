50d22d8863950d15258df5d04c432e5e687da2cc
Oliver Palmer <oliverpalmer@opalmer.com>
2014-03-02 13:04:09 -0500

    removing unused float range generation (there's better ways to do this and it should not be in this module anyway)

--- tests/test_utility.py
+++ tests/test_utility.py
@@ -16,17 +16,11 @@
 
 from __future__ import with_statement
 
-import math
 from json import loads
-
-try:
-    _range = xrange
-except NameError:
-    _range = range
 
 from pyfarm.core.testutil import TestCase
 from pyfarm.core.enums import Values
-from pyfarm.core.utility import float_range, convert, rounded, dumps
+from pyfarm.core.utility import convert, dumps
 
 
 class Convert(TestCase):
@@ -47,72 +41,6 @@ class Convert(TestCase):
 
         with self.assertRaises(ValueError):
             convert.ston("foo")
-
-
-class Rounding(TestCase):
-    def test_rounded(self):
-        self.assertEqual(rounded(math.pi), 3.1416)
-        self.assertEqual(rounded(math.pi, 2), 3.14)
-        self.assertEqual(rounded(math.pi, 6), 3.141593)
-
-    def test_rounded_places_type_error(self):
-        with self.assertRaises(TypeError):
-            rounded(1.5, None)
-
-    def test_rounded_places_count(self):
-        with self.assertRaises(ValueError):
-            rounded(1.5, 0)
-
-
-class Range(TestCase):
-    def test_range_end_error(self):
-        with self.assertRaises(ValueError):
-            float_range(2, 1)
-
-    def test_range_by_error(self):
-        with self.assertRaises(ValueError):
-            float_range(5, by=0)
-
-    def test_intrange_start(self):
-        self.assertEqual(list(float_range(5)), list(_range(5)))
-
-    def test_intrangestartby(self):
-        self.assertEqual(list(float_range(5, by=1)), [0, 1, 2, 3, 4])
-        self.assertEqual(list(float_range(5, by=2)), [0, 2, 4])
-
-    def test_intrange_startendby(self):
-        self.assertEqual(list(float_range(1, 1, 1)), list(_range(1, 1, 1)))
-        self.assertEqual(list(float_range(1, 10, 1)), list(_range(1, 10, 1)))
-        self.assertEqual(list(float_range(1, 10, 2)), list(_range(1, 10, 2)))
-
-    def test_floatrange_startby(self):
-        self.assertEqual(
-            list(float_range(2.25, by=.25)),
-            [0, 0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 2.25])
-        self.assertEqual(list(float_range(2, by=.25)),
-            [0, 0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0])
-        self.assertEqual(list(float_range(2, by=.75)), [0, 0.75, 1.5])
-        self.assertEqual(list(float_range(2, by=.75, add_endpoint=True)),
-            [0, 0.75, 1.5, 2.0])
-        self.assertEqual(list(float_range(2.5, by=.15)),
-            [0, 0.15, 0.3, 0.45, 0.6, 0.75, 0.9, 1.05, 1.2, 1.35, 1.5,
-             1.65, 1.8, 1.95, 2.1, 2.25, 2.4])
-
-    def test_floatrange_start(self):
-        self.assertEqual(list(list(float_range(2.25))), [0, 1, 2])
-        self.assertEqual(list(list(float_range(2.25, add_endpoint=True))),
-                         [0, 1, 2, 2.25])
-
-    def test_floatrange_startend(self):
-        self.assertEqual(list(float_range(2.25, 5)), [2.25, 3.25, 4.25])
-        self.assertEqual(list(float_range(2.25, 5, add_endpoint=True)),
-                         [2.25, 3.25, 4.25, 5])
-
-    def test_floatrange_startendby(self):
-        self.assertEqual(list(float_range(1.5, 2.5, .15)),
-            [1.5, 1.65, 1.8, 1.95, 2.1, 2.25, 2.4])
-        self.assertEqual(list(float_range(1.5, 2.5, .15, add_endpoint=True)),
-            [1.5, 1.65, 1.8, 1.95, 2.1, 2.25, 2.4, 2.5])
 
 
 class JSONDumper(TestCase):
