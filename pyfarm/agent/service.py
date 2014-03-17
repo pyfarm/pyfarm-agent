6daf1c52ce679178f3bbc0b0092f2815e0d8edc6
Oliver Palmer <oliverpalmer@opalmer.com>
2014-03-17 00:11:31 -0400

    agent adding self to master (wip)

--- pyfarm/agent/service.py
+++ pyfarm/agent/service.py
@@ -24,22 +24,20 @@ such as log reading, system information gathering, and management of processes.
 
 import json
 import time
-import logging
 from random import random
 from datetime import datetime
-from functools import partial
+from httplib import NOT_FOUND, BAD_REQUEST, OK
 
 import ntplib
 import requests
 from twisted.internet import reactor
 from twisted.application.service import MultiService
 from twisted.internet.error import ConnectionRefusedError
-from twisted.python import log
 
 from pyfarm.core.enums import AgentState
 from pyfarm.core.logger import getLogger
 from pyfarm.core.sysinfo import memory
-from pyfarm.agent.http.client import post
+from pyfarm.agent.http.client import post, get
 from pyfarm.agent.tasks import ScheduledTaskManager, memory_utilization
 from pyfarm.agent.process.manager import ProcessManager
 from pyfarm.agent.config import config
@@ -54,11 +52,6 @@ class ManagerService(MultiService):
 
     def __init__(self):
         self.scheduled_tasks = ScheduledTaskManager()
-        self.log = partial(log.msg, system=self.__class__.__name__)
-        self.info = partial(self.log, level=logging.INFO)
-        self.error = partial(self.log, level=logging.ERROR)
-        self.exception = partial(log.err, system=self.__class__.__name__)
-
         # register any scheduled tasks
         self.scheduled_tasks.register(
             memory_utilization, config["memory-check-interval"],
@@ -118,11 +111,30 @@ class ManagerService(MultiService):
 
 
 class Agent(object):
+    TIME_OFFSET = config["time-offset"]
     scheduled_tasks = ScheduledTaskManager()
     ntp_client = ntplib.NTPClient()
 
     def __init__(self):
         self.first_post_attempts = 1
+
+    @classmethod
+    def agents_api(cls):
+        """Return the top level API url for all agents"""
+        return config["master-api"] + "/agents/"
+
+    @classmethod
+    def agent_api(cls):
+        """
+        Return the API url for this agent or None if `agent-id` has not
+        been set
+        """
+        try:
+            return cls.agents_api() + str(config["agent-id"])
+        except KeyError:
+            svclog.error(
+                "The `agent-id` configuration value has not been set yet")
+            return None
 
     @classmethod
     def http_retry_delay(cls):
@@ -132,35 +144,38 @@ class Agent(object):
         return delay
 
     @classmethod
-    def get_system_data(cls):
+    def system_data(cls, requery_timeoffset=False):
         """
         Returns a dictionary of data containing information about the
         agent.  This is the information that is also passed along to
         the master.
         """
-        ntplog.debug(
-            "querying ntp server %r for current time", config["ntp-server"])
-        try:
-            pool_time = cls.ntp_client.request(config["ntp-server"])
+        # query the time offset and then cache it since
+        # this is typically a blocking operation
+        if requery_timeoffset or cls.TIME_OFFSET is None:
+            ntplog.info(
+                "Querying ntp server %r for current time", config["ntp-server"])
 
-        except Exception, e:
-            time_offset_int = 0
-            ntplog.warning("failed to determine network time: %s", e)
+            try:
+                pool_time = cls.ntp_client.request(config["ntp-server"])
 
-        else:
-            time_offset_int = int(pool_time.tx_time - time.time())
+            except Exception as e:
+                ntplog.warning("Failed to determine network time: %s", e)
 
-            # format the offset for logging purposes
-            utcoffset = datetime.utcfromtimestamp(pool_time.tx_time)
-            iso_timestamp = utcoffset.isoformat()
-            ntplog.debug(
-                "network time: %s (local offset: %r)",
-                iso_timestamp, time_offset_int)
+            else:
+                cls.TIME_OFFSET = int(pool_time.tx_time - time.time())
 
-            if time_offset_int:
-                ntplog.warning(
-                    "agent is %r second(s) off from ntp server at %r",
-                    time_offset_int, config["ntp-server"])
+                # format the offset for logging purposes
+                utcoffset = datetime.utcfromtimestamp(pool_time.tx_time)
+                iso_timestamp = utcoffset.isoformat()
+                ntplog.debug(
+                    "network time: %s (local offset: %r)",
+                    iso_timestamp, cls.TIME_OFFSET)
+
+                if cls.TIME_OFFSET:
+                    ntplog.warning(
+                        "Agent is %r second(s) off from ntp server at %r",
+                        cls.TIME_OFFSET, config["ntp-server"])
 
         data = {
             "hostname": config["hostname"],
@@ -170,7 +185,7 @@ class Agent(object):
             "cpus": config["cpus"],
             "port": config["port"],
             "free_ram": memory.ram_free(),
-            "time_offset": time_offset_int,
+            "time_offset": cls.TIME_OFFSET or 0,
             "state": AgentState.ONLINE}
 
         if config.get("remote-ip"):
@@ -181,40 +196,123 @@ class Agent(object):
 
         return data
 
+    def search_for_agent(self, run=True):
+        """
+        Produces a callable object which will initiate the process
+        necessary to search for this agent.  This is a method on the class
+        itself so we can repeat the search from any location.
+        """
+        def search():
+            system_data = self.system_data()
+            return get(
+                self.agents_api(),
+                callback=self.callback_search_for_agent,
+                errback=self.errback_search_for_agent,
+                params={
+                    "hostname": system_data["hostname"],
+                    "ip": system_data["ip"]})
+
+        return search() if run else search
+
+    def create_agent(self, run=True):
+        """Creates a new agent on the master"""
+        def create():
+            svclog.info("Registering this agent with the master")
+            return post(
+                self.agents_api(),
+                callback=self.callback_create_agent,
+                errback=self.errback_create_agent,
+                data=self.system_data())
+
+        return create() if run else create
+
+    def callback_create_agent(self, response):
+        print "===============", response
+
+    def errback_create_agent(self, failure):
+        delay = self.http_retry_delay()
+        svclog.warning(
+            "There was a problem creating the agent: %s.  Retrying "
+            "in %r seconds", failure, delay)
+        reactor.callLater(delay, self.create_agent(run=False))
+
+    def callback_search_for_agent(self, response):
+        delay = self.http_retry_delay()
+
+        if response.code == OK:
+            system_data = self.system_data()
+            svclog.debug(
+                "This agent may already be registered with the master")
+
+            # see if one of the agents found is in fact us
+            agents = response.json()
+            similiar_agents = []
+            if isinstance(agents, list):
+                for agent in agents:
+                    match_ip = \
+                        agent["ip"] == system_data["ip"]
+                    match_hostname = \
+                        agent["hostname"] == system_data["hostname"]
+                    match_port = agent["port"] == config["port"]
+
+                    # this agent matches exactly, setup the configuration
+                    if match_ip and match_hostname and match_port:
+                        svclog.info(
+                            "This agent is registered with the master, its "
+                            "id is %r.", agent["id"])
+                        config["agent-id"] = agent["id"]
+                        break
+
+                    # close but it's the wrong port
+                    elif match_ip and match_hostname:
+                        similiar_agents.append(agent)
+
+                else:
+                    if similiar_agents:
+                        svclog.info(
+                            "There are similar agents registered with the "
+                            "master but this agent is not.")
+
+        elif response.code == NOT_FOUND:
+            svclog.debug(
+                "This agent is not currently registered with the master.")
+            self.create_agent()
+
+        elif config >= BAD_REQUEST:
+            svclog.warning(
+                "Something was either wrong with our request or the "
+                "server cannot handle it at this time: %r.  Retrying in "
+                "%s seconds", response.data(), delay)
+            reactor.callLater(delay, lambda: response.request.retry())
+
+        # Retry anyway otherwise we could end up with the agent doing
+        # nothing.
+        else:
+            svclog.error(
+                "Unhandled case while attempting to find registered "
+                "agent.  Retrying in %s seconds", delay)
+            reactor.callLater(delay, lambda: response.request.retry())
+
+    def errback_search_for_agent(self, failure):
+        delay = self.http_retry_delay()
+        agents_api = self.agents_api()
+
+        if failure.type is ConnectionRefusedError:
+            svclog.warning(
+                "Connection refused to %s, retrying in %s seconds",
+                agents_api, delay)
+
+        else:
+            # TODO: need a better way of making these errors visible
+            svclog.critical(
+                "Unhandled exception, retrying in %s seconds", delay)
+            svclog.exception(failure)
+
+        reactor.callLater(delay, self.search_for_agent(run=False))
+
     def run(self):
-        system_data = self.get_system_data()
-        url = config["master-api"] + "/agents/"
-
-        def callback(response):
-            print response
-
-        def errback(failure):
-            # TODO: max number of first post attempts may not be the same
-            # as --http-max-retries
-            if self.first_post_attempts > config["http-max-retries"]:
-                svclog.critical(
-                    "Reached maximum number of attempts to announce "
-                    "the agent's presence to %r", url)
-                reactor.stop()
-
-            self.first_post_attempts += 1
-            delay = self.http_retry_delay()
-            do_post = lambda: post(
-                config["master-api"] + "/agents/",
-                data=system_data, callback=callback, errback=errback)
-
-            if failure.type is ConnectionRefusedError:
-                svclog.warning(
-                    "Connection refused to %s, retrying in %s seconds",
-                    url, delay)
-                reactor.callLater(delay, do_post)
-            else:
-                svclog.critical("Unhandled exception, stopping reactor")
-                svclog.exception(failure)
-                reactor.stop()
-
-        # fire off the first POST
-        post(
-            config["master-api"] + "/agents/",
-            data=system_data, callback=callback, errback=errback)
-
+        """
+        Internal code which starts the agent, registers it with the master,
+        and performs the other steps necessary to get things running.
+        """
+        self.search_for_agent()
