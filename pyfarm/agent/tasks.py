c754846747056c1619ca41799de6728d4d66a83f
Oliver Palmer <oliverpalmer@opalmer.com>
2014-03-20 22:09:26 -0400

    memory_utilization() needs an update and should not live here anyway

--- pyfarm/agent/tasks.py
+++ pyfarm/agent/tasks.py
@@ -29,7 +29,6 @@ from functools import partial
 from twisted.python import log
 from twisted.internet.task import LoopingCall
 
-from pyfarm.core.sysinfo import memory
 
 memlog = partial(log.msg, system="task.memory_utilization")
 
@@ -117,27 +116,3 @@ class ScheduledTaskManager(object):
                 looping_call.stop()
             else:
                 self.log("...%s is already stopped" % function.__name__)
-
-
-# TODO: only send memory information if memory has risen by X amount in N time
-# TODO: replace with callable class (to support the above)
-def memory_utilization(config):  # pragma: no cover
-    """
-    Returns the amount of free free and the amount of swap used.
-    """
-    try:
-        ram_report_delta = config["ram_report_delta"]
-        ram_record_delta = config["ram_record_delta"]
-        swap_report_delta = config["swap_report_delta"]
-        swap_record_delta = config["swap_record_delta"]
-
-    except KeyError:
-        # TODO: handle this better, this was added to just stop being annoying
-        import warnings
-        warnings.warn("configuration not available yet")
-        return
-
-    ram_free, swap_used = memory.ram_free(), memory.swap_used()
-    log.msg("ram_free=%s, swap_used=%s" % (ram_free, swap_used),
-            system="task.memory_utilization")
-    return ram_free, swap_used
