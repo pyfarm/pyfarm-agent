059e09615d793ef11d3211d6614c5ffc19af4776
Oliver Palmer <oliverpalmer@opalmer.com>
2014-04-12 21:33:04 -0400

    finishing up resource resource tests

--- tests/test_agent/test_http_core_resource.py
+++ tests/test_agent/test_http_core_resource.py
@@ -14,14 +14,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
+from StringIO import StringIO
+
 try:
-    from httplib import BAD_REQUEST, INTERNAL_SERVER_ERROR
-except ImportError:
-    from httplib.client import BAD_REQUEST, INTERNAL_SERVER_ERROR
+    from httplib import (
+        BAD_REQUEST, INTERNAL_SERVER_ERROR, NOT_FOUND, UNSUPPORTED_MEDIA_TYPE)
+except ImportError:  # pragma: no cover
+    from httplib.client import (
+        BAD_REQUEST, INTERNAL_SERVER_ERROR, NOT_FOUND, UNSUPPORTED_MEDIA_TYPE)
 
 from twisted.web.test.requesthelper import DummyRequest as _DummyRequest
 from twisted.web.http import Headers
 from twisted.web.server import NOT_DONE_YET
+from voluptuous import Schema, Required
 
 from pyfarm.core.enums import STRING_TYPES
 from pyfarm.agent.testutil import TestCase
@@ -30,11 +36,18 @@ from pyfarm.agent.http.core.template import DeferredTemplate
 from pyfarm.agent.utility import dumps
 
 
+class DummyContent(StringIO):
+    def read(self, n=-1):
+        return self.getvalue()
+
+
 class DummyRequest(_DummyRequest):
-    def __init__(self, uri=None, headers=None):
+    def __init__(self, uri=None, headers=None, data=None, method="GET"):
         _DummyRequest.__init__(self, uri or [""])
         self.data = ""
         self.requestHeaders = Headers()
+        self.content = DummyContent()
+        self.method = method.upper()
 
         if headers is not None:
             for key, value in headers.items():
@@ -44,6 +57,11 @@ class DummyRequest(_DummyRequest):
 
         if uri is not None:
             self.path = self.uri = uri
+
+        if data is not None:
+            if not isinstance(data, STRING_TYPES):
+                data = dumps(data)
+            self.content.write(data)
 
     def write(self, data):
         if not isinstance(data, STRING_TYPES):
@@ -62,7 +80,22 @@ class ResourceWithDuplicateMethods(Resource):
     render_GET = lambda *args, **kwargs: None
 
 
-class TestResourceInternals(TestCase):
+class PostResource(Resource):
+    def __init__(self):
+        Resource.__init__(self)
+        self.kwargs = {}
+
+    def post(self, **kwargs):
+        self.kwargs.update(kwargs)
+        return self.kwargs
+
+
+class PostResourceWithSchema(PostResource):
+    SCHEMAS = {
+        "POST": Schema({Required("foo"): int})}
+
+
+class TestResourceBase(TestCase):
     def setUp(self):
         TestCase.setUp(self)
         self._template = Resource.TEMPLATE
@@ -73,6 +106,8 @@ class TestResourceInternals(TestCase):
         Resource.TEMPLATE = self._template
         Resource.CONTENT_TYPES = self._content_types
 
+
+class TestResourceInternals(TestResourceBase):
     def test_classvar_template(self):
         self.assertIs(Resource.TEMPLATE, NotImplemented)
 
@@ -152,7 +187,7 @@ class TestResourceInternals(TestCase):
         request = DummyRequest(headers={"content-type": "foobar"})
         resource = Resource()
         resource.error(request, BAD_REQUEST, "internal_error")
-        self.assertEqual(request.responseCode, INTERNAL_SERVER_ERROR)
+        self.assertEqual(request.responseCode, UNSUPPORTED_MEDIA_TYPE)
         self.assertTrue(request.finished)
         self.assertEqual(
             request.data,
@@ -160,11 +195,94 @@ class TestResourceInternals(TestCase):
                        "Can only handle text/html or application/json here"}))
 
 
-class TestResourceRendering(TestCase):
+
+class TestResourceRendering(TestResourceBase):
     def test_leaf_without_trailing_slash(self):
-        request = DummyRequest("")
+        request = DummyRequest("/foo", headers={"content-type": "application/json"})
         resource = Resource()
         resource.isLeaf = True
         render_result = resource.render(request)
         self.assertEqual(render_result, NOT_DONE_YET)
-        # results
+        self.assertEqual(request.data, dumps({"error": "/foo does not exist"}))
+        self.assertEqual(request.responseCode, NOT_FOUND)
+
+    def test_invalid_content_type(self):
+        request = DummyRequest("", headers={"content-type": "foo"})
+        resource = Resource()
+        render_result = resource.render(request)
+        self.assertEqual(render_result, NOT_DONE_YET)
+        self.assertEqual(
+            request.data,
+            dumps({"error":
+                       "Can only handle text/html or application/json here"}))
+        self.assertEqual(request.responseCode, UNSUPPORTED_MEDIA_TYPE)
+
+    def test_no_content_provided_in_request(self):
+        request = DummyRequest(
+            "", headers={"content-type": "application/json"},
+            data="", method="POST")
+        resource = PostResource()
+        render_result = resource.render(request)
+        self.assertEqual(render_result, NOT_DONE_YET)
+        self.assertEqual(request.data, dumps({"error": "No data provided"}))
+        self.assertEqual(request.responseCode, BAD_REQUEST)
+
+    def test_post_json(self):
+        data = {os.urandom(6).encode("hex"): os.urandom(6).encode("hex")}
+        request = DummyRequest(
+            "", headers={"content-type": "application/json"},
+            data=data, method="POST")
+        resource = PostResource()
+        render_result = resource.render(request)
+        self.assertIs(render_result["request"], request)
+        self.assertEqual(render_result["data"], data)
+
+    def test_post_json_decode_error(self):
+        request = DummyRequest(
+            "", headers={"content-type": "application/json"},
+            data="{", method="POST")
+        resource = PostResource()
+        render_result = resource.render(request)
+        self.assertEqual(render_result, NOT_DONE_YET)
+        self.assertEqual(
+            request.data,
+            dumps({
+                "error": "Failed to decode json data: ValueError('Expecting "
+                         "object: line 1 column 1 (char 0)',)"}))
+        self.assertEqual(request.responseCode, BAD_REQUEST)
+
+    def test_post_schema(self):
+        data = {"foo": 42}
+        request = DummyRequest(
+            "", headers={"content-type": "application/json"},
+            data=data, method="POST")
+        resource = PostResourceWithSchema()
+        render_result = resource.render(request)
+        self.assertIs(render_result["request"], request)
+        self.assertEqual(render_result["data"], data)
+
+    def test_post_schema_error(self):
+        data = {"foo": "bar"}
+        request = DummyRequest(
+            "", headers={"content-type": "application/json"},
+            data=data, method="POST")
+        resource = PostResourceWithSchema()
+        render_result = resource.render(request)
+        self.assertEqual(render_result, NOT_DONE_YET)
+        self.assertEqual(request.data, dumps({
+            "error": "Failed to validate the request data against "
+                     "the schema: expected int for dictionary value @ "
+                     "data[u'foo']"}))
+        self.assertEqual(request.responseCode, BAD_REQUEST)
+
+    def test_unsupported_method(self):
+        data = {"foo": "bar"}
+        request = DummyRequest(
+            "", headers={"content-type": "application/json"},
+            data=data, method="DELETE")
+        resource = PostResource()
+        render_result = resource.render(request)
+        self.assertEqual(render_result, NOT_DONE_YET)
+        self.assertEqual(request.data, dumps({
+            "error": "'' only supports the POST, HEAD method(s)"}))
+
