91f1da28c638d16c98611ac850b62e86f18e125e
Oliver Palmer <oliverpalmer@opalmer.com>
2014-04-15 00:17:19 -0400

    work in progress job type cache

--- pyfarm/agent/utility.py
+++ pyfarm/agent/utility.py
@@ -24,10 +24,27 @@ are copied over from the master (which we can't import here).
 
 from decimal import Decimal
 from datetime import datetime
-from json import dumps as _dumps
+from json import dumps as _dumps, loads
 from UserDict import UserDict
 
+try:
+    import zlib
+except ImportError:
+    zlib = NotImplemented
+
+try:
+    INTEGERS = (int, long)
+except NameError:
+    INTEGERS = int
+
+from twisted.internet import threads
+
+from pyfarm.core.config import read_env_bool
+from pyfarm.core.enums import STRING_TYPES
+from pyfarm.core.logger import getLogger
 from pyfarm.agent.config import config
+
+logger = getLogger("agent.utility")
 
 
 def default_json_encoder(obj):
@@ -53,3 +70,63 @@ def dumps(*args, **kwargs):
         obj = dict(*args, **kwargs)
 
     return _dumps(obj, default=default_json_encoder, indent=indent)
+
+
+class JobTypeCache(object):
+    """
+    This class is responsible for retrieval and caching of job types.  This
+    object is simply a small interface to the config object.  It's
+    provided so the way we store job types can be handled in a single
+    location and so that we can control the structure from a couple of
+    places instead of the whole project.
+
+    Additionally class is part of the config library because when the config
+    object is dumped to disk, which can sometimes happen as part of the
+    shutdown sequence,
+    """
+    COMPRESSED = zlib is not NotImplemented and read_env_bool(
+        "PYFARM_AGENT_COMPRESS_JOBTYPES", True)
+
+    @classmethod
+    def unpack(cls, jobtype, version):
+        """
+        The internal method used to retrieve a specific job type
+        and version which is then directly returned.  This function
+        generally should not be directly executed and instead :func:`.get`
+        should be used as that will also handle retrieval of missing job
+        types.
+
+        :returns:
+            Returns a dictionary structure containing all the
+            information necessary to execute a job type.
+        """
+        assert "jobtypes" in config
+        assert (jobtype, version) in config["jobtypes"]
+        logger.debug("Unpacking job type %r version %r", jobtype, version)
+        data = config["jobtypes"][(jobtype, version)]
+
+        # If the data is being stored as a string due to compression
+        # then package the
+        if cls.COMPRESSED:
+            data = loads(zlib.decompress(data))
+
+        return data
+
+    @classmethod
+    def get(cls, jobtype, version):
+        """
+        Returns a deferred object which will either return the
+        cached job type or retrieve the job type, cache it, and
+        then return it.
+        """
+        assert isinstance(jobtype, STRING_TYPES)
+        assert isinstance(version, INTEGERS)
+
+        # Job type is cached, defer the unpacking to a thread
+        # so any decompression won't block the reactor
+        if (jobtype, version) in config["jobtypes"]:
+            return threads.deferToThread(cls.unpack, jobtype, version)
+
+        # TODO: retrieve job type
+        # TODO: cache
+        # TODO: return result by firing deferred (which resulted form REST call)
