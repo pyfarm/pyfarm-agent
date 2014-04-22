5008eeb4620e98e87f68a89cf5c04328fb48f7c7
Oliver Palmer <oliverpalmer@opalmer.com>
2014-04-21 23:13:41 -0400

    unicode csv readers/writers

--- pyfarm/agent/utility.py
+++ pyfarm/agent/utility.py
@@ -22,6 +22,9 @@ Top level utilities for the agent to use internally.  Many of these
 are copied over from the master (which we can't import here).
 """
 
+import csv
+import codecs
+import cStringIO
 from decimal import Decimal
 from datetime import datetime
 from json import dumps as _dumps
@@ -62,3 +65,66 @@ def dumps(*args, **kwargs):
         obj = dict(*args, **kwargs)
 
     return _dumps(obj, default=default_json_encoder, indent=indent)
+
+# Unicode CSV reader/writers from the standard library docs:
+#   https://docs.python.org/2/library/csv.html
+
+
+class UTF8Recoder(object):
+    """
+    Iterator that reads an encoded stream and reencodes the input to UTF-8
+    """
+    def __init__(self, f, encoding):
+        self.reader = codecs.getreader(encoding)(f)
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        return self.reader.next().encode("utf-8")
+
+
+class UnicodeCSVReader(object):
+    """
+    A CSV reader which will iterate over lines in the CSV file "f",
+    which is encoded in the given encoding.
+    """
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        f = UTF8Recoder(f, encoding)
+        self.reader = csv.reader(f, dialect=dialect, **kwds)
+
+    def next(self):
+        row = self.reader.next()
+        return [unicode(s, "utf-8") for s in row]
+
+    def __iter__(self):
+        return self
+
+
+class UnicodeCSVWriter(object):
+    """
+    A CSV writer which will write rows to CSV file "f",
+    which is encoded in the given encoding.
+    """
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        # Redirect output to a queue
+        self.queue = cStringIO.StringIO()
+        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
+        self.stream = f
+        self.encoder = codecs.getincrementalencoder(encoding)()
+
+    def writerow(self, row):
+        self.writer.writerow([s.encode("utf-8") for s in row])
+        # Fetch UTF-8 output from the queue ...
+        data = self.queue.getvalue()
+        data = data.decode("utf-8")
+        # ... and reencode it into the target encoding
+        data = self.encoder.encode(data)
+        # write to the target stream
+        self.stream.write(data)
+        # empty queue
+        self.queue.truncate(0)
+
+    def writerows(self, rows):
+        for row in rows:
+            self.writerow(row)
