3ffac940fd09a793234a8f57df7bba5bfe78b334
Oliver Palmer <oliverpalmer@opalmer.com>
2014-04-27 11:38:43 -0400

    fixing unittests

--- tests/test_agent/test_config.py
+++ tests/test_agent/test_config.py
@@ -43,6 +43,7 @@ class ChangedLoggingConfiguration(LoggingConfiguration):
 class TestLoggingConfiguration(TestCase):
     def get_data(self):
         return {
+            "jobtypes": {},
             urandom(16).encode("hex"): urandom(16).encode("hex"),
             urandom(16).encode("hex"): urandom(16).encode("hex")}
 
