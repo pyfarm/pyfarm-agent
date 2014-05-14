1d39c24fe72cec1f0dd64deb0b241a49de17433c
Guido Winkelmann <guido@ambient-entertainment.de>
2014-05-14 10:39:40 +0200

    GET /tasks: synchronous output
    We are not actually doing anything asynchronous here, so we might as go
    the simpler synchronous route for the output

--- pyfarm/agent/http/api/tasks.py
+++ pyfarm/agent/http/api/tasks.py
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from json import dumps
+
 from twisted.web.server import NOT_DONE_YET
 
 from pyfarm.agent.config import config
@@ -27,6 +29,4 @@ class Tasks(APIResource):
         for assignment in config["current_assignments"].itervalues():
             tasks += assignment["tasks"]
 
-        request.write(tasks)
-        request.finish()
-        return NOT_DONE_YET
+        return dumps(data)
