e82f4f3660ecb5d03e2b268ac35ede939feb30a4
Oliver Palmer <oliverpalmer@opalmer.com>
2014-05-18 11:02:29 -0400

    adding service_identity module which will verify hostnames

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ from setuptools import setup
 
 install_requires = [
     "pyfarm.core",
-    "PyOpenSSL",  # required for https support
+    "PyOpenSSL", "service_identity",  # required for full SSL support
     "netaddr", "twisted", "ntplib", "requests", "treq",
     "voluptuous", "jinja2", "psutil>=2.1.0",
     "netifaces>=0.10.2"]
