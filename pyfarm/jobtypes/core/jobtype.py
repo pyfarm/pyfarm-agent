baaf6df2d60f5c4d131ba9d4c602955530e404f3
Oliver Palmer <oliverpalmer@opalmer.com>
2014-06-01 23:18:52 -0400

    moving logging thread into its own module

--- pyfarm/jobtypes/core/jobtype.py
+++ pyfarm/jobtypes/core/jobtype.py
@@ -43,15 +43,14 @@ from twisted.internet.defer import Deferred
 from twisted.internet.error import ProcessDone, ProcessTerminated
 from twisted.python.failure import Failure
 
-from pyfarm.core.config import (
-    read_env, read_env_float, read_env_int, read_env_bool)
+from pyfarm.core.config import read_env, read_env_bool
 from pyfarm.core.enums import WINDOWS, INTEGER_TYPES, STRING_TYPES, WorkState
 from pyfarm.core.logger import getLogger
 from pyfarm.core.utility import ImmutableDict
 from pyfarm.agent.config import config
 from pyfarm.agent.http.core.client import get, post, http_retry_delay
 from pyfarm.agent.sysinfo.user import is_administrator
-from pyfarm.agent.utility import UnicodeCSVWriter
+from pyfarm.jobtypes.core.log import STDERR, STDOUT, LoggingThread
 from pyfarm.jobtypes.core.process import (
     ProcessProtocol, ProcessInputs, ReplaceEnvironment)
 
@@ -77,75 +76,6 @@ assert isinstance(DEFAULT_ENVIRONMENT, dict)
 
 DEFAULT_CACHE_DIRECTORY = read_env(
     "PYFARM_JOBTYPE_CACHE_DIRECTORY", ".jobtypes")
-STDOUT = 0
-STDERR = 1
-STREAMS = set([STDOUT, STDERR])
-
-
-# TODO: if we get fail the task if we have errors
-class LoggingThread(threading.Thread):
-    """
-    This class runs a thread which writes lines in csv format
-    to the log file.
-    """
-    FLUSH_AFTER_LINES = read_env_int(
-        "PYFARM_JOBTYPE_LOGGING_FLUSH_AFTER_LINES", 50)
-    QUEUE_GET_TIMEOUT = read_env_float(
-        "PYFARM_JOBTYPE_LOGGING_QUEUE_TIMEOUT", .25)
-
-    def __init__(self, filepath):
-        super(LoggingThread, self).__init__()
-        self.queue = Queue()
-        self.stream = open(filepath, "wb")
-        self.writer = UnicodeCSVWriter(self.stream)
-        self.lineno = 1
-        self.next_flush = self.FLUSH_AFTER_LINES
-        self.stopped = False
-        self.shutdown_event = \
-            reactor.addSystemEventTrigger("before", "shutdown", self.stop)
-
-    def put(self, streamno, message):
-        """Put a message in the queue for the thread to pickup"""
-        assert self.stopped is False, "Cannot put(), thread is stopped"
-        assert streamno in STREAMS
-        assert isinstance(message, STRING_TYPES)
-        now = datetime.utcnow()
-        self.queue.put_nowait(
-            (now.isoformat(), streamno, self.lineno, message))
-        self.lineno += 1
-
-    def stop(self):
-        self.stopped = True
-        reactor.removeSystemEventTrigger(self.shutdown_event)
-
-    def run(self):
-        stopping = False
-        while True:
-            # Pull data from the queue or retry again
-            try:
-                timestamp, streamno, lineno, message = self.queue.get(
-                    timeout=self.QUEUE_GET_TIMEOUT)
-            except Empty:
-                pass
-            else:
-                # Write data from the queue to a file
-                self.writer.writerow(
-                    [timestamp, str(streamno), str(lineno), message])
-                if self.lineno >= self.next_flush:
-                    self.stream.flush()
-                    self.next_flush += self.FLUSH_AFTER_LINES
-
-            # We're either being told to stop or we
-            # need to run one more iteration of the
-            # loop to pickup any straggling messages.
-            if self.stopped and stopping:
-                logger.debug("Closing %s", self.stream.name)
-                self.stream.close()
-                break
-
-            # Go around one more time to pickup remaining messages
-            elif self.stopped:
-                stopping = True
 
 
 class JobType(object):
