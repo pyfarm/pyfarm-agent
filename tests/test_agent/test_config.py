52682460336850325d49991005c144e7f7156350
Oliver Palmer <oliverpalmer@opalmer.com>
2014-06-15 11:35:23 -0400

    removing unused import

--- tests/test_agent/test_config.py
+++ tests/test_agent/test_config.py
@@ -14,7 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from datetime import datetime
 from os import urandom
 
 from pyfarm.core.enums import NOTSET
