15a8d8cc7b18674ccabe509c6d9b64a623aa9259
Oliver Palmer <oliverpalmer@opalmer.com>
2014-06-26 00:36:53 -0400

    tests for ProcessProtocol properties

--- tests/test_jobtypes/test_core_process.py
+++ tests/test_jobtypes/test_core_process.py
@@ -17,10 +17,88 @@
 import os
 from collections import namedtuple
 
+import psutil
+from twisted.internet import reactor
+from twisted.internet.defer import Deferred
+from twisted.internet.protocol import ProcessProtocol as _ProcessProtocol
+
 from pyfarm.agent.testutil import TestCase
-from pyfarm.jobtypes.core.process import ReplaceEnvironment
+from pyfarm.jobtypes.core.process import ReplaceEnvironment, ProcessProtocol
 
 DummyInputs = namedtuple("DummyInputs", ("task", ))
+
+
+class FakeJobType(object):
+    def __init__(self):
+        self.done = Deferred()
+        self.started = False
+        self.stopped = False
+        self.stdout = []
+        self.stderr = []
+
+    def process_started(self, protocol):
+        self.started = True
+
+    def process_stopped(self, protocol, reason):
+        self.stopped = True
+        self.done.callback(True)
+
+    def received_stdout(self, data):
+        self.stdout.append(data)
+
+    def received_stderr(self, data):
+        self.stderr.append(data)
+
+
+class TestProtocol(TestCase):
+    def launch_process(self, jobtype, args=None):
+        if args is None:
+            args = ["python", "-c", "import time; time.sleep(3600)"]
+
+        protocol = ProcessProtocol(jobtype, *[None] * 6)
+        reactor.spawnProcess(
+            protocol, "python", args)
+        return protocol
+
+    def test_subclass(self):
+        protocol = ProcessProtocol(*[None] * 7)
+        self.assertIsInstance(protocol, _ProcessProtocol)
+
+    def test_pid(self):
+        fake_jobtype = FakeJobType()
+        protocol = self.launch_process(fake_jobtype)
+        self.assertEqual(protocol.pid, protocol.transport.pid)
+        protocol.transport.signalProcess("KILL")
+        return fake_jobtype.done
+
+    def test_process(self):
+        fake_jobtype = FakeJobType()
+        protocol = self.launch_process(fake_jobtype)
+        self.assertIs(protocol.process, protocol.transport)
+        protocol.transport.signalProcess("KILL")
+        return fake_jobtype.done
+
+    def test_psutil_process_after_exit(self):
+        fake_jobtype = FakeJobType()
+        protocol = self.launch_process(fake_jobtype)
+
+        def exited(*_):
+            self.assertIsNone(protocol.psutil_process)
+
+        fake_jobtype.done.addCallback(exited)
+        protocol.transport.signalProcess("KILL")
+
+        return fake_jobtype.done
+
+    def test_psutil_process_running(self):
+        fake_jobtype = FakeJobType()
+        protocol = self.launch_process(fake_jobtype)
+        self.assertIsInstance(protocol.psutil_process, psutil.Process)
+        self.assertEqual(protocol.psutil_process.pid, protocol.pid)
+        protocol.transport.signalProcess("KILL")
+        return fake_jobtype.done
+
+    # TODO: add tests for remaining protocol attributes
 
 
 class TestReplaceEnvironment(TestCase):
@@ -60,11 +138,3 @@ class TestReplaceEnvironment(TestCase):
             pass
 
         self.assertEqual(os.environ, original_copy)
-
-
-class TestProcessProtocol(TestCase):
-    def test_process_id_from_task(self):
-        pass
-        # protocol = ProcessProtocol(
-        #     None, None, DummyInputs(task=42), None, None, None, None, None,
-        #     None)
