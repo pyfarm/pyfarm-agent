1d3afa6e6132e0e7ca45d1e725c32db81c25960e
Oliver Palmer <oliverpalmer@opalmer.com>
2014-06-30 00:19:52 -0400

    use remote-ip, fix free_ram calc

--- tests/test_agent/test_service.py
+++ tests/test_agent/test_service.py
@@ -25,6 +25,7 @@ from pyfarm.agent.sysinfo.system import system_identifier
 from pyfarm.agent.testutil import TestCase, PYFARM_AGENT_MASTER
 from pyfarm.agent.config import config
 from pyfarm.agent.service import Agent
+from pyfarm.agent.sysinfo import memory
 
 
 # TODO: need better tests, these are a little rudimentary at the moment
@@ -49,13 +50,12 @@ class TestAgentBasicMethods(TestCase):
             "ram": config["ram"],
             "cpus": config["cpus"],
             "port": config["port"],
-            "free_ram": config["free-ram"],
+            "free_ram": int(memory.ram_free()),
             "time_offset": config["time-offset"],
             "state": config["state"]}
 
         agent = Agent()
-        self.assertEqual(agent.system_data(), expected)
-        config["remote-ip"] = expected["remote_ip"] = \
+        config["remote-ip"] = expected["remote-ip"] = \
             os.urandom(16).encode("hex")
         self.assertEqual(agent.system_data(), expected)
 
