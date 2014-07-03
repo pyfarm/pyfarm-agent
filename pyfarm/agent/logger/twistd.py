59dd98b821361e394e87c867a3236535d57ca393
Oliver Palmer <oliverpalmer@opalmer.com>
2014-07-02 23:04:19 -0400

    added text color, formatting and output

--- pyfarm/agent/logger/twistd.py
+++ pyfarm/agent/logger/twistd.py
@@ -22,22 +22,55 @@ Agent specific logging library that combines some of
 Twisted's and Python's logging facilities.
 """
 
-from logging import DEBUG, _levelNames
 from collections import deque
+from datetime import datetime
+from logging import DEBUG, WARNING, CRITICAL, ERROR, FATAL, _levelNames
 
 from twisted.python.log import textFromEventDict
 
+from pyfarm.core.enums import INTERACTIVE_INTERPRETER
+
+
+# Only setup colorama if we're not inside
+# of an interpreter.
+if not INTERACTIVE_INTERPRETER:
+    try:
+        colorama
+    except NameError:
+        from colorama import init, Fore, Back, Style
+        init()
+
 
 class Observer(object):
-    SETUP = False
+    INSTANCE = None
+
+    if not INTERACTIVE_INTERPRETER:
+        FORMATS = {
+            DEBUG: (Style.DIM, Style.RESET_ALL),
+            WARNING: (Fore.YELLOW, Fore.RESET),
+            ERROR: (Fore.RED, Fore.RESET),
+            CRITICAL: (
+                Fore.RED + Style.BRIGHT, Fore.RESET + Style.RESET_ALL),
+            FATAL: (
+                Fore.RED + Style.BRIGHT, Fore.RESET + Style.RESET_ALL)}
+
+        def add_color(self, text, level):
+            head, tail = self.FORMATS.get(level, DEBUG)
+            return head + text + tail
+    else:
+        FORMATS = {}
+        def add_color(self, text, _):
+            return text
 
     def __init__(self):
         self.backlog = deque()
         self.datefmt = None
+        self.format = None
         self.levels = []
 
     def configure(self, config):
         self.datefmt = config["logging"]["datefmt"]
+        self.format = config["logging"]["format"]
 
         for name, level in config["logging"]["levels"]:
             if not isinstance(level, int):
@@ -50,21 +83,30 @@ class Observer(object):
 
             self.levels.append((name, level))
 
-        Observer.SETUP = True
-
-    def handle_event(self, event):
-        arguments = event.get("args", ())
-        level = event.get("logLevel", DEBUG)
-
-        system = event.get("system", "twisted")
-        if system == "-":
-            system = "twisted"
-
+    def emit(self, event):
+        # Get the message
         text = textFromEventDict(event)
         if text is None or text == "Log opened.":
             return
 
-        # print text % event["args"]
+        # Create a timestamp
+        if "time" in event:
+            asctime = datetime.fromtimestamp(
+                event["time"]).strftime(self.datefmt)
+        else:
+            # Since we're going to display the time we don't
+            # want UTC here.
+            asctime = datetime.now().strftime(self.datefmt)
+
+        levelno = event.get("logLevel", DEBUG)
+        levelname = _levelNames[levelno]
+        name = event.get("system", "twisted")
+        message = text % event.get("args", ())
+
+        if name == "-":
+            name = "twisted"
+
+        print self.add_color(self.format % locals(), levelno)
 
     def __call__(self, event):
         """
@@ -73,20 +115,17 @@ class Observer(object):
         """
         # If we have not been fully setup yet then
         # processing any messages would not
-        if not self.SETUP:
+        if self.format is None:
             self.backlog.append(event)
             return
 
         # If we have anything built up in the backlog
         # then be sure we handle these first
+        print "HERE"
         while self.backlog:
             try:
-                self.handle_event(self.backlog.pop())
+                self.emit(self.backlog.pop())
             except IndexError:
                 break
 
-        self.handle_event(event)
-
-
-
-
+        self.emit(event)
