b1c9b3000a7eabfb9d0f3dace6783c0050eb9771
Oliver Palmer <oliverpalmer@opalmer.com>
2014-07-03 21:26:57 -0400

    couple of bugfixes and doc. strings for twistd

--- pyfarm/agent/logger/twistd.py
+++ pyfarm/agent/logger/twistd.py
@@ -15,13 +15,17 @@
 # limitations under the License.
 
 """
-Logger
-------
+Twisted Logger
+--------------
 
-Agent specific logging library that combines some of
-Twisted's and Python's logging facilities.
+Provides facilities for handling logging events
+in a manner which is consistent with Twisted's normal
+way of handling log events.
 """
 
+from __future__ import print_function
+
+import sys
 from collections import deque
 from datetime import datetime
 from fnmatch import fnmatch
@@ -32,6 +36,7 @@ from twisted.python.log import textFromEventDict
 from pyfarm.core.enums import INTERACTIVE_INTERPRETER
 
 CONFIGURATION = {
+    "output": "stdout",
     "datefmt": "%Y-%m-%d %H:%M:%S",
     "format": "%(asctime)s %(levelname)-8s - %(name)-15s - %(message)s",
 
@@ -40,8 +45,7 @@ CONFIGURATION = {
       # should be defined using *'s to define matches.
     "levels": [
         ("", DEBUG),
-        ("HTTP11ClientProtocol*", INFO)
-    ]}
+        ("HTTP11ClientProtocol*", INFO)]}
 
 # Only setup colorama if we're not inside
 # of an interpreter.
@@ -54,6 +58,10 @@ if not INTERACTIVE_INTERPRETER:
 
 
 class Observer(object):
+    """
+    Main class which provides handling and output
+    of log events.
+    """
     INSTANCE = None
 
     if not INTERACTIVE_INTERPRETER:
@@ -82,32 +90,55 @@ class Observer(object):
         self.backlog = deque()
         self.datefmt = None
         self.format = None
+        self.output = None
         self.levels = []
 
     def configure(self):
+        """
+        Configures the logger using the :const:`CONFIGURATION` dictionary
+        in this module.  This establishes the
+        """
+        if CONFIGURATION["output"] == "stdout":
+            self.output = sys.stdout
+        elif CONFIGURATION["output"] == "stderr":
+            self.output = sys.stderr
+        else:
+            raise NotImplementedError(
+                "Don't know how to use %r for output" % CONFIGURATION["output"])
+
         self.datefmt = CONFIGURATION["datefmt"]
         self.format = CONFIGURATION["format"]
 
-        for name, level in CONFIGURATION["levels"]:
-            if not isinstance(level, int):
-                try:
-                    level = _levelNames[level.upper()]
-
-                except KeyError:
-                    raise KeyError(
-                        "%r's level %r does not exist" % (name, level))
-
-            self.levels.append((name, level))
-
-    def filter(self, name, level):
         for fname, flevel in CONFIGURATION["levels"]:
-            if fname == "" and level > flevel:
+            if fname == "":
+                self.max_level = flevel
+                break
+
+    def filter(self, name, level, message):
+        """
+        Return ``True`` if the given log line should be
+        filtered out.
+        """
+        for fname, flevel in CONFIGURATION["levels"]:
+            if level > self.max_level or level > flevel:
                 return True
 
             if (fname == name or fnmatch(name, fname)) and flevel > level:
                 return True
 
+        if message == "Log opened.":
+            return True
+
     def emit(self, event):
+        """
+        Every log message that is produced from from either
+        a Python logger or :class:`pyfarm.agent.logger.twistd.Logger`
+        will be passed into this method.
+
+        This method will take the logging event, format it, filters
+        if necessary and then writes the message to stdout or stderr
+        depending on the configuration.
+        """
         # Get the message
         text = textFromEventDict(event)
         if text is None:
@@ -130,24 +161,26 @@ class Observer(object):
         if name == "-":
             name = "twisted"
 
-        if self.filter(name, levelno):
+        if self.filter(name, levelno, message):
             return
 
-        print self.add_color(self.format % locals(), levelno)
+        print(self.add_color(self.format % locals(), levelno),
+              file=self.output)
 
     def __call__(self, event):
         """
-        Handles all logging events which are passed into Twisted's logging
-        system.
+        Handles all logging events which are passed into Twisted's
+        logging system.  This will queue logging messages until
+        :class:`Observer` has been configured.  Once the observer has
+        been configured however logging messages will be passed along
+        to :meth:`emit`.
         """
-        # If we have not been fully setup yet then
-        # processing any messages would not
-        if self.format is None:
+        # Only queue messages if we don't have something
+        # to output to.
+        if self.output is None:
             self.backlog.append(event)
             return
 
-        # If we have anything built up in the backlog
-        # then be sure we handle these first
         while self.backlog:
             try:
                 self.emit(self.backlog.pop())
