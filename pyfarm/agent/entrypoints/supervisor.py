02d0a2d2683ee7305107ae6299e3f6bfd14e3368
Oliver Palmer <oliverpalmer@opalmer.com>
2014-07-04 19:08:35 -0400

    chroot -> chdir (since it's not really a chroot), add in SetConfig action

--- pyfarm/agent/entrypoints/supervisor.py
+++ pyfarm/agent/entrypoints/supervisor.py
@@ -69,18 +69,23 @@ def supervisor():
         description="Start and monitor the agent process")
     parser.add_argument("--updates-drop-dir",
                         default=config["agent_updates_dir"],
-                        action=partial(SetConfig, key="agent_updates_dir"),
+                        action=partial(
+                            SetConfig, key="agent_updates_dir", isfile=True),
                         help="Where to look for agent updates")
     parser.add_argument("--agent-package-dir",
                         help="Path to the actual agent code")
     parser.add_argument("--pidfile", default=config["supervisor_lock_file"],
+                        action=partial(
+                            SetConfig, key="supervisor_lock_file", isfile=True),
                         help="The file to store the process id in. "
                              "[default: %(default)s]")
     parser.add_argument("-n", "--no-daemon", default=False, action="store_true",
                         help="If provided then do not run the process in the "
                              "background.")
-    parser.add_argument("--chroot",
-                        help="The directory to chroot to upon launch.")
+    parser.add_argument("--chdir", default=config["agent_chdir"],
+                        action=partial(
+                            SetConfig, key="agent_chdir", isfile=True),
+                        help="The directory to chdir to upon launch.")
     parser.add_argument("--uid", type=int,
                         help="The user id to run the supervisor as.  "
                              "*This setting is ignored on Windows.*")
@@ -92,7 +97,7 @@ def supervisor():
     if not args.no_daemon and fork is not NotImplemented:
         logger.info("sending supervisor log output to %s" % args.log)
         daemon_start_return_code = start_daemon_posix(
-            args.log, args.chroot, args.uid, args.gid)
+            args.log, args.chdir, args.uid, args.gid)
 
         if isinstance(daemon_start_return_code, INTEGER_TYPES):
             return daemon_start_return_code
