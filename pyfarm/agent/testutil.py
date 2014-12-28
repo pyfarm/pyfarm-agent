1a04f3bd59af6f9775a4defd449f72d80d7c24ad
Oliver Palmer <oliverpalmer@opalmer.com>
2014-12-27 23:29:19 -0500

    add jobtype_ignore_id_mapping_errors config value

--- pyfarm/agent/testutil.py
+++ pyfarm/agent/testutil.py
@@ -398,7 +398,8 @@ class TestCase(_TestCase):
             "start": time.time(),
             "agent_pretty_json": False,
             "agent_html_template_reload": True,
-            "agent_master_reannounce": randint(5, 15)})
+            "agent_master_reannounce": randint(5, 15),
+            "jobtype_ignore_id_mapping_errors": True})
 
     def _rmdir(self, path, on_exit=True):
         try:
