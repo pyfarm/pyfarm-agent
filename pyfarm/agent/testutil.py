1cb764a5b1990da5e536f7db6593f1844c18dc4e
Oliver Palmer <oliverpalmer@opalmer.com>
2015-03-08 23:07:03 -0400

    whitespace

--- pyfarm/agent/testutil.py
+++ pyfarm/agent/testutil.py
@@ -559,6 +559,7 @@ class BaseHTTPTestCase(TestCase):
 
 class BaseAPITestCase(BaseHTTPTestCase):
     DEFAULT_HEADERS = {"Accept": ["application/json"]}
+
     def test_parent(self):
         self.assertIsInstance(self.instance_class(), APIResource)
 
