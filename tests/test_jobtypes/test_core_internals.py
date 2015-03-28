042040bc128540ff54511f6a3406029edb6b272c
Oliver Palmer <oliverpalmer@opalmer.com>
2015-03-28 19:34:55 -0400

    convert methods -> static methods (don't need the class)
    Also removing some old tests which no longer apply or have
    been replaced by other tests.

--- tests/test_jobtypes/test_core_internals.py
+++ tests/test_jobtypes/test_core_internals.py
@@ -262,120 +262,60 @@ class TestProcess(TestCase):
                         "username", "get_uid", pwd, "pwd"))
 
 
-# TODO: fix these tests to use the new CommandData.validate
-# class TestSpawnProcessTypeChecks(TestCase):
-    # def test_command(self):
-    #     checks = TypeChecks()
-    #     with self.assertRaisesRegexp(TypeError, ".*for.*command.*"):
-    #         checks._check_spawn_process_inputs(
-    #             None, None, None, None, None, None)
-    #
-    # def test_arguments(self):
-    #     checks = TypeChecks()
-    #     with self.assertRaisesRegexp(TypeError, ".*for.*arguments.*"):
-    #         checks._check_spawn_process_inputs(
-    #             "", None, None, None, None, None)
-    #
-    # def test_workdir_not_string(self):
-    #     checks = TypeChecks()
-    #     with self.assertRaisesRegexp(TypeError, ".*for.*working_.*"):
-    #         checks._check_spawn_process_inputs(
-    #             "", [], None, None, None, None)
-    #
-    # def test_workdir_missing(self):
-    #     checks = TypeChecks()
-    #     with self.assertRaises(OSError):
-    #         checks._check_spawn_process_inputs(
-    #             "", [], urandom(8).encode("hex"), {}, "foo", "foo")
-    #
-    # def test_environment(self):
-    #     checks = TypeChecks()
-    #     with self.assertRaisesRegexp(TypeError, ".*for.*environment.*"):
-    #         checks._check_spawn_process_inputs(
-    #             "", [], gettempdir(), None, None, None)
-    #
-    # def test_user(self):
-    #     checks = TypeChecks()
-    #     with self.assertRaisesRegexp(TypeError, ".*for.*user.*"):
-    #         checks._check_spawn_process_inputs(
-    #             "", [], gettempdir(), {}, bool, None)
-    #
-    # def test_group(self):
-    #     checks = TypeChecks()
-    #     with self.assertRaisesRegexp(TypeError, ".*for.*group.*"):
-    #         checks._check_spawn_process_inputs(
-    #             "", [], gettempdir(), {}, 0, bool)
-    #
-    # @skipIf(is_administrator() or WINDOWS, "Cannot run on Windows or as root")
-    # def test_cannot_change_user(self):
-    #     checks = TypeChecks()
-    #     with self.assertRaisesRegexp(EnvironmentError, ".*change user or.*"):
-    #         checks._check_spawn_process_inputs(
-    #             "", [], gettempdir(), {}, "foo", "foo")
-
-
-class TestMiscTypeChecks(TestCase):
+class TestTypeChecks(TestCase):
     def test_expandvars_value_not_string(self):
-        checks = TypeChecks()
-        checks._check_expandvars_inputs("", {})
+        TypeChecks._check_expandvars_inputs("", {})
 
         with self.assertRaisesRegexp(TypeError,
                                      re.compile(".*for.*value.*")):
-            checks._check_expandvars_inputs(None, None)
+            TypeChecks._check_expandvars_inputs(None, None)
 
     def test_expandvars_environment_not_dict(self):
-        checks = TypeChecks()
-        checks._check_expandvars_inputs("", None)
+        TypeChecks._check_expandvars_inputs("", None)
 
         with self.assertRaisesRegexp(TypeError,
                                      re.compile(".*for.*environment.*")):
-            checks._check_expandvars_inputs("", 1)
+            TypeChecks._check_expandvars_inputs("", 1)
 
     def test_map(self):
-        checks = TypeChecks()
         for objtype in STRING_TYPES:
-            checks._check_map_path_inputs(objtype())
+            TypeChecks._check_map_path_inputs(objtype())
 
         with self.assertRaisesRegexp(TypeError, re.compile(".*for.*path.*")):
-            checks._check_map_path_inputs(None)
+            TypeChecks._check_map_path_inputs(None)
 
     def test_csvlog_path_tasks(self):
-        checks = TypeChecks()
-        checks._check_csvlog_path_inputs(uuid4(), None)
+        TypeChecks._check_csvlog_path_inputs(uuid4(), None)
 
         with self.assertRaisesRegexp(
                 TypeError, re.compile("Expected UUID for `protocol_uuid`")):
-            checks._check_csvlog_path_inputs(None, None)
+            TypeChecks._check_csvlog_path_inputs(None, None)
 
     def test_csvlog_path_time(self):
-        checks = TypeChecks()
-        checks._check_csvlog_path_inputs(uuid4(), None)
+        TypeChecks._check_csvlog_path_inputs(uuid4(), None)
 
         with self.assertRaisesRegexp(
                 TypeError, re.compile("Expected UUID for `protocol_uuid`")):
-            checks._check_csvlog_path_inputs([], "")
+            TypeChecks._check_csvlog_path_inputs([], "")
 
     def test_command_list(self):
-        checks = TypeChecks()
-        checks._check_command_list_inputs(tuple())
-        checks._check_command_list_inputs([])
+        TypeChecks._check_command_list_inputs(tuple())
+        TypeChecks._check_command_list_inputs([])
 
         with self.assertRaisesRegexp(TypeError, re.compile(".*for.*cmdlist.*")):
-            checks._check_command_list_inputs(None)
+            TypeChecks._check_command_list_inputs(None)
 
     def test_set_state_tasks(self):
-        checks = TypeChecks()
         for objtype in ITERABLE_CONTAINERS:
-            checks._check_set_states_inputs(objtype(), WorkState.DONE)
+            TypeChecks._check_set_states_inputs(objtype(), WorkState.DONE)
 
         with self.assertRaisesRegexp(TypeError, re.compile(".*for.*tasks.*")):
-            checks._check_set_states_inputs(None, None)
+            TypeChecks._check_set_states_inputs(None, None)
 
     def test_set_state_state(self):
-        checks = TypeChecks()
         for state in WorkState:
-            checks._check_set_states_inputs(ITERABLE_CONTAINERS[0](), state)
+            TypeChecks._check_set_states_inputs(ITERABLE_CONTAINERS[0](), state)
 
         with self.assertRaisesRegexp(ValueError,
                                      re.compile(".*Expected.*state.*")):
-            checks._check_set_states_inputs(ITERABLE_CONTAINERS[0](), None)
+            TypeChecks._check_set_states_inputs(ITERABLE_CONTAINERS[0](), None)
