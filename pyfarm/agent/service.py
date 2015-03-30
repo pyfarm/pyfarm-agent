784c2ba10a2671e20267fe9fae885c3c175b28d7
Oliver Palmer <oliverpalmer@opalmer.com>
2015-03-29 21:00:20 -0400

    remove unused stop_reactor keyword

--- pyfarm/agent/service.py
+++ pyfarm/agent/service.py
@@ -588,7 +588,7 @@ class Agent(object):
         deferred.addCallbacks(lambda _: reactor.stop(), errback)
 
     @inlineCallbacks
-    def post_shutdown_to_master(self, stop_reactor=True):
+    def post_shutdown_to_master(self):
         """
         This method is called before the reactor shuts down and lets the
         master know that the agent's state is now ``offline``
