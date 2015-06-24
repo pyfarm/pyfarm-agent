854a2a001207799fa334c74def65ff62259e8441
Guido Winkelmann <guido@ambient-entertainment.de>
2015-06-24 22:01:39 +0200

    Finish tests for check software API

--- tests/test_agent/test_http_api_software.py
+++ tests/test_agent/test_http_api_software.py
@@ -18,20 +18,18 @@
 import json
 
 try:
-    from httplib import OK
+    from httplib import OK, ACCEPTED, NO_CONTENT
 except ImportError:  # pragma: no cover
-    from http.client import OK
+    from http.client import OK, ACCEPTED, NO_CONTENT
 
 from twisted.web.resource import Resource
 from twisted.internet.defer import inlineCallbacks
+from twisted.internet import reactor
 from twisted.web.server import Site, NOT_DONE_YET
 
 from pyfarm.agent.http.api.software import CheckSoftware
-from pyfarm.agent.testutil import BaseAPITestCase
-
-
-class SoftwareAPIRoot(Resource):
-    isLeaf = False
+from pyfarm.agent.testutil import BaseAPITestCase, random_port
+from pyfarm.agent.config import config
 
 
 class FakeSoftwareVersionAPI(Resource):
@@ -42,12 +40,12 @@ class FakeSoftwareVersionAPI(Resource):
 
     def render_GET(self, request):
         request.setResponseCode(OK)
-        request.write({
+        request.write(json.dumps({
             "version": "1.0",
             "id": 1,
             "rank": 100,
             "discovery_function_name": "check_example_sw"
-            })
+            }))
         request.finish()
         return NOT_DONE_YET
 
@@ -57,12 +55,61 @@ class FakeSoftwareVersionCodeAPI(Resource):
 
     def __init__(self):
         Resource.__init__(self)
+        self.code = ""
 
     def render_GET(self, request):
         request.setResponseCode(OK)
-        request.write("def check_example_sw():\n"
-                      "    return True\n")
+        request.write(self.code)
         request.finish()
+        return NOT_DONE_YET
+
+
+class AgentAPIRoot(Resource):
+    isLeaf = False
+
+    def getChild(self, name, request):
+        return Resource.getChild(self, name, request)
+
+
+class FakeAgentAPI(Resource):
+    isLeaf = False
+
+    def getChild(self, name, request):
+        return Resource.getChild(self, name, request)
+
+
+class FakeAgentSoftwareAPI(Resource):
+    isLeaf = False
+
+    def __init__(self):
+        Resource.__init__(self)
+        self.times_posted = 0
+
+    def getChild(self, name, request):
+        if request.postpath == []:
+            return self
+        else:
+            return Resource.getChild(self, name, request)
+
+    def render_POST(self, request):
+        request.setResponseCode(OK)
+        request.write(request.content.read())
+        request.finish()
+        self.times_posted += 1
+        return NOT_DONE_YET
+
+
+class FakeAgentSoftwareVersionAPI(Resource):
+    isLeaf = True
+
+    def __init__(self):
+        Resource.__init__(self)
+        self.times_deleted = 0
+
+    def render_DELETE(self, request):
+        request.setResponseCode(NO_CONTENT)
+        request.finish()
+        self.times_deleted += 1
         return NOT_DONE_YET
 
 
@@ -79,33 +126,93 @@ class TestCheckSoftware(BaseAPITestCase):
     def setUp(self):
         super(TestCheckSoftware, self).setUp()
         self.resource = Resource()
-        sw_api_root = SoftwareAPIRoot()
-        api_root_resource = self.resource.putChild("api/v1/software",
-                                                   sw_api_root)
-        self.fake_software_version_api = FakeSoftwareVersionAPI()
-        version_resource = api_root_resource.putChild(
-            "example_sw/versions/1.0", self.fake_software_version_api)
+        sw_api_root = Resource()
+        self.resource.putChild("software", sw_api_root)
+
+        fake_software_api = Resource()
+        sw_api_root.putChild("example_sw", fake_software_api)
+
+        fake_version_index_api = Resource()
+        fake_software_api.putChild("versions", fake_version_index_api)
+
+        self.fake_version_api = FakeSoftwareVersionAPI()
+        fake_version_index_api.putChild("1.0", self.fake_version_api)
+
         self.fake_discovery_code_api = FakeSoftwareVersionCodeAPI()
-        version_resource.putChild("discovery_code",
-                                  self.fake_discovery_code_api)
+        self.fake_version_api.putChild(
+            "discovery_code", self.fake_discovery_code_api)
+
+        agent_api_root = AgentAPIRoot()
+        self.resource.putChild("agents", agent_api_root)
+
+        fake_agent_api = FakeAgentAPI()
+        agent_api_root.putChild(str(config.get("agent_id")), fake_agent_api)
+
+        self.fake_agent_software_api = FakeAgentSoftwareAPI()
+        fake_agent_api.putChild("software", self.fake_agent_software_api)
+
+        agent_example_sw_api = Resource()
+        self.fake_agent_software_api.putChild("example_sw",
+                                              agent_example_sw_api)
+
+        agent_example_sw_version_index_api = Resource()
+        agent_example_sw_api.putChild("versions",
+                                      agent_example_sw_version_index_api)
+
+        self.agent_example_sw_version_1_0_api = FakeAgentSoftwareVersionAPI()
+        agent_example_sw_version_index_api.putChild(
+            "1.0",
+            self.agent_example_sw_version_1_0_api)
+
         self.site = Site(self.resource)
         self.server = reactor.listenTCP(random_port(), self.site)
         config["master_api"] = "http://127.0.0.1:%s" % self.server.port
 
     @inlineCallbacks
     def tearDown(self):
-        super(TestCheckSoftware(), self).tearDown()
+        super(TestCheckSoftware, self).tearDown()
         yield self.server.loseConnection()
 
     def test_check_software(self):
+        self.fake_discovery_code_api.code = ("def check_example_sw():\n"
+                                             "    return True\n")
         request = self.post(
             data={
                 "software": "example_sw",
                 "version": "1.0"
                 },
             headers={"User-Agent": config["master_user_agent"]})
-        check = self.instance_class()
-        result = check.render(request)
+        check_software = self.instance_class()
+        check_software.testing = True
+        result = check_software.render(request)
         self.assertEqual(result, NOT_DONE_YET)
         self.assertTrue(request.finished)
         self.assertEqual(request.responseCode, ACCEPTED)
+        check_software.operation_deferred.addCallback(
+            lambda _: self.assertEqual(
+                self.fake_agent_software_api.times_posted, 1)
+        )
+
+        return check_software.operation_deferred
+
+    def test_check_software_not_existing(self):
+        self.fake_discovery_code_api.code = ("def check_example_sw():\n"
+                                             "    return False\n")
+        request = self.post(
+            data={
+                "software": "example_sw",
+                "version": "1.0"
+                },
+            headers={"User-Agent": config["master_user_agent"]})
+        check_software = self.instance_class()
+        check_software.testing = True
+        result = check_software.render(request)
+        self.assertEqual(result, NOT_DONE_YET)
+        self.assertTrue(request.finished)
+        self.assertEqual(request.responseCode, ACCEPTED)
+        check_software.operation_deferred.addCallback(
+            lambda _: self.assertEqual(
+                self.agent_example_sw_version_1_0_api.times_deleted, 1)
+        )
+
+        return check_software.operation_deferred
