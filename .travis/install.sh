d8664d8a84c8c481dcc48aa6137c1bbf6416a02d
Oliver Palmer <oliverpalmer@opalmer.com>
2015-09-02 21:39:45 -0400

    remove SSL test (for now), install project

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,22 +1,4 @@
-#!/bin/bash -ex
-
-function retry {
-    local attempt=1
-    local command="${@}"
-
-    while [ $attempt -ne 10 ]; do
-        echo "(run $attempt/10) running $command"
-        $command
-        if [[ $? -eq 0 ]]; then
-            echo "(success) $command"
-            break
-        else
-            ((attempt++))
-            echo "(FAILED) $command"
-            sleep 3
-        fi
-    done
-}
+#!/bin/bash -e
 
 if [[ "$(uname -s)" == 'Darwin' ]]; then
     brew update || brew update
@@ -39,11 +21,8 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
     esac
     pyenv rehash
     pyenv activate pyfarm-agent
-else
-    retry pip install coverage python-coveralls mock --quiet
-    if [[ $PYFARM_AGENT_TEST_HTTP_SCHEME == "https" ]]; then
-        retry pip install PyOpenSSL service_identity --quiet
-    fi
-    retry pip install -e . --egg --quiet
-    pip freeze
 fi
+
+pip install coverage python-coveralls mock --quiet
+pip install -e . --egg --quiet
+pip freeze
