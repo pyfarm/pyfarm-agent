02f6140c910a835742871a3db827514d207f3109
Oliver Palmer <oliverpalmer@opalmer.com>
2015-09-02 22:30:46 -0400

    adding missing rehash/activate steps

--- .travis/install.sh
+++ .travis/install.sh
@@ -11,6 +11,8 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
 
     pyenv install $PYTHON_VERSION
     pyenv virtualenv $PYTHON_VERSION virtualenv
+    pyenv rehash
+    pyenv activate virtualenv
 fi
 
 pip install coverage python-coveralls mock
