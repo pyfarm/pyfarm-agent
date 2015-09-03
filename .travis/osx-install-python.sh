c012e5f4bbd855571cef5969ecc445948a9acf4e
Oliver Palmer <oliverpalmer@opalmer.com>
2015-09-02 22:08:37 -0400

    run commands directly in .travis.yml

--- .travis/osx-install-python.sh
+++ .travis/osx-install-python.sh
@@ -4,13 +4,11 @@ brew update || brew update
 brew outdated pyenv || brew upgrade pyenv
 brew install pyenv-virtualenv
 
-if which pyenv > /dev/null; then
-    eval "$(pyenv init -)"
-fi
+if which pyenv > /dev/null; then eval "$(pyenv init -)" fi
 
 case "${TRAVIS_PYTHON_VERSION}" in
     2.6)
-        pyenv install 2.6.9
+        pyenv install $PYTHON_VERSION
         pyenv virtualenv 2.6.9 pyfarm-agent
     ;;
     2.7)
@@ -18,8 +16,3 @@ case "${TRAVIS_PYTHON_VERSION}" in
         pyenv virtualenv 2.7.10 pyfarm-agent
     ;;
 esac
-
-pyenv rehash
-pyenv activate pyfarm-agent
-
-
