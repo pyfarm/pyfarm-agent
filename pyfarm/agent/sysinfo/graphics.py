80b52fd19b64856606b014c1800c22a53ee44b18
Oliver Palmer <oliverpalmer@opalmer.com>
2015-09-22 23:40:45 -0400

    adding 'no cover' for Windows and Mac at the moment.  We should come up with a better way of doing this though, at the very least it would probably be beneficial to never allow 'no cover' and only enable it when using coveralls/travis

--- pyfarm/agent/sysinfo/graphics.py
+++ pyfarm/agent/sysinfo/graphics.py
@@ -53,7 +53,7 @@ def graphics_cards():
     """
     Returns a list of the full names of GPUs installed in this system
     """
-    if WINDOWS:
+    if WINDOWS:  # pragma: no cover
         wmi = WMI()
         gpus = wmi.Win32_VideoController.query()
         gpu_names = [x.name for x in gpus]
@@ -81,7 +81,7 @@ def graphics_cards():
 
         return gpu_names
 
-    elif MAC:
+    elif MAC:  # pragma: no cover
         try:
             profiler_pipe = Popen(
                 ["system_profiler", "-xml", "SPDisplaysDataType"], stdout=PIPE)
