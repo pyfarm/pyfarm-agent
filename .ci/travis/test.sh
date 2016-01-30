c5c3671894ffe04cacd93ec97571361bbe69c046
Oliver Palmer <oliverpalmer@opalmer.com>
2016-01-30 10:55:21 -0500

    upload code coverage

--- .ci/travis/test.sh
+++ .ci/travis/test.sh
@@ -2,15 +2,18 @@
 
 if [[ "$READTHEDOCS" == "1" ]]; then
     make -C docs html
-fi
-
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    if which pyenv > /dev/null; then
-        eval "$(pyenv init -)"
-    fi
-    pyenv activate virtualenv
-    coverage run --branch $VIRTUAL_ENV/bin/trial tests
-
 else
-    coverage run --branch `which trial` tests/
+    if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+        if which pyenv > /dev/null; then
+            eval "$(pyenv init -)"
+        fi
+        pyenv activate virtualenv
+        coverage run --branch $VIRTUAL_ENV/bin/trial tests
+    else
+        coverage run --branch `which trial` tests/
+    fi
+
+    # Upload code coverage to codecov.io
+    coverage xml
+    codecov
 fi
